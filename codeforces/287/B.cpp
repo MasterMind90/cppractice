1070cd62b2bc98eeb0b93d0d9bce59aaeae7f615
Amjad Alhouseini <amjadm2008@gmail.com>
2013-03-25 10:50:00 +0200

    Add solution for problem `B - Pipeline` Link: https://codeforces.com/contest/287/problem/B Tags: binary search, math, *1700 Ref: https://codeforces.com/contest/287/submission/3402914

--- codeforces/287/B.cpp
+++ codeforces/287/B.cpp
@@ -1,39 +1,16 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-typedef long long ll ;
-ll n , k ;
-ll sum(ll n){
-    return (n*(n+1))/2LL;
-}
-bool p(ll x){
-    ll ans = sum(k) - sum(x-1);
-    if ( ans >= n ) return true ;
-    return false;
-}
-
+unsigned long long n,sum;
+long k,i;
 int main(){
-    cin >> n >> k ;
-    if ( n == 1 ) {
-        cout << 0 << endl;
-        return 0 ;
+    cin >> n >> k;
+    sum=1;
+    while(k!=1 && sum<n){
+        sum+=k-1;
+        i++;
+        k--;
     }
-    n--;
-    k--;
-    if ( sum(k) < n ){
-        cout << -1 << endl;
-        return 0 ;
-    }
-    ll l = 1 ;
-    ll r = k ;
-    while(l<r){
-        ll mid = l + (r-l+1)/2;
-        if ( p(mid) == false ){
-            r = mid - 1 ;
-        }
-        else {
-            l = mid ;
-        }
-    }
-    cout << k-l+1 << endl;
-    return 0 ;
+    if (sum>=n)cout << i << endl;
+    else cout << -1 << endl;
+    return 0;
 }
