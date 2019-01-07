b60d0cca25a4cd49745a776578e718e06e926d4d
Amjad Alhouseini <amjadm2008@gmail.com>
2019-01-07 13:54:00 +0200

    Add solution for problem `B - Petr and a Combination Lock` Link: https://codeforces.com/contest/1097/problem/B Tags: bitmasks, brute force, dp, *1200 Ref: https://codeforces.com/contest/1097/submission/48055212

--- codeforces/1097/B.cpp
+++ codeforces/1097/B.cpp
@@ -1,26 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+int clockwise(int cur,int x){
+    return (cur+x)%360;
+}
+int counterwise(int curr,int x){
+    if ( curr-x < 0 ) {
+        return 360+(curr-x);
+    }
+    return curr-x;
+}
 int main(){
     int n ;
     cin >> n ;
-    vector<int> v(n);
+    vector<int> v(n) ;
     for(int i=0;i<n;i++){
         cin >> v[i];
     }
-    for(int i=0;i<(1<<n);i++){
+    for(int mask=0;mask<(1<<n);mask++){
         int sum = 0 ;
-        for(int j=0;j<n;j++){
-            if ( (1<<j) & i ){
-                sum += v[j];
+        for(int i=0;i<n;i++){
+            if ( (1<<i) & mask ){
+                sum = clockwise(sum,v[i]);
             }
-            else sum -= v[j];
+            else sum  = counterwise(sum,v[i]);
         }
-        if ( sum % 360 == 0 ) {
-            cout << "yes" << endl;
+        if ( sum == 0 ) {
+            cout << "YES" << endl;
             return 0 ;
         }
     }
-    cout << "no" << endl;
+    cout << "NO" << endl;
     return 0 ;
 }
