68bed0d92bc2de46a023a33ad9827dbdc4bd12b8
Amjad Alhouseini <amjadm2008@gmail.com>
2019-01-24 12:53:00 +0200

    Add solution for problem `C - Nice Garland` Link: https://codeforces.com/contest/1108/problem/C Tags: brute force, greedy, math, *1300 Ref: https://codeforces.com/contest/1108/submission/48882311

--- codeforces/1108/C.cpp
+++ codeforces/1108/C.cpp
@@ -2,30 +2,40 @@
 using namespace std;
 
 int main(){
+    string s[6] = {"RGB","RBG","GBR","GRB","BGR","BRG"};
     int n ;
     cin >> n ;
     string x ;
     cin >> x ;
-    string cur = "RGB" ;
-    sort(cur.begin(),cur.end());
+    int len = n/3 ;
+    if ( n % 3 != 0 ) len++;
     int mc = 1e9 ;
     string ans = "" ;
-    do{
+    for(int i=0;i<6;i++){
+        string tmp = "" ;
+        for(int j=0;j<len;j++){
+            tmp += s[i];
+        }
         int cnt = 0 ;
         for(int i=0;i<x.size();i++){
-            if ( cur[i%3] != x[i] ) cnt++;
+            if ( x[i] != tmp[i] ) cnt++;
         }
         if ( cnt < mc ) {
             mc = cnt ;
-            ans = cur ;
+            ans = tmp;
         }
-    }while(next_permutation(cur.begin(),cur.end()));
-
-    cout << mc << endl;
-    for(int i=0;i<n;i++){
-        cout << ans[i%3] ;
     }
-    cout << endl;
+    cout << mc << endl;
+    if ( n % 3 == 1 ){
+        cout << ans.substr(0,ans.size()-2);
+    }
+    else if ( n % 3 == 2 ){
+        cout << ans.substr(0,ans.size()-1) ;
+    }
+    else {
+        cout << ans ;
+    }
+    cout << endl ;
 
     return 0 ;
 }
