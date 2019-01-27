83ef859c7bc8b8bdfa0b0974d0fc7dbee5a3eb2c
Amjad Alhouseini <amjadm2008@gmail.com>
2019-01-27 10:49:00 +0200

    Add solution for problem `C - Brutality` Link: https://codeforces.com/contest/1107/problem/C Tags: greedy, sortings, two pointers, *1300 Ref: https://codeforces.com/contest/1107/submission/49046036

--- codeforces/1107/C.cpp
+++ codeforces/1107/C.cpp
@@ -10,21 +10,22 @@ int main(){
     }
     string x ;
     cin >> x ;
-    priority_queue<long long> q ;
-    q.push(v[0]);
+    vector<long long> ans ;
+    ans.push_back(v[0]);
     long long res = 0 ;
     for(int i=1;i<=x.size();i++){
         if ( x[i] != x[i-1] ){
-            long long cnt = (long long)q.size() ;
-            for(int i=0;i<min(k,cnt);i++){
-                res += q.top() ; q.pop();
+            sort(ans.begin(),ans.end());
+            reverse(ans.begin(),ans.end());
+            for(int i=0;i<min(k,(long long)ans.size());i++){
+                res+=ans[i];
             }
-            q = priority_queue<long long>() ;
+            ans.clear();
             if ( i == x.size() ) continue ;
-            q.push(v[i]);
+            ans.push_back(v[i]);
         }
         else{
-            q.push(v[i]);
+            ans.push_back(v[i]);
         }
     }
     cout << res << endl;
