77cba7746531e9d55983b48cc4e8d039f1212765
Amjad Alhouseini <amjadm2008@gmail.com>
2019-02-02 14:16:00 +0200

    Add solution for problem `A - Vasya and Book` Link: https://codeforces.com/contest/1082/problem/A Tags: implementation, math, *1200 Ref: https://codeforces.com/contest/1082/submission/49358970

--- codeforces/1082/A.cpp
+++ codeforces/1082/A.cpp
@@ -1,35 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long
+
+#define ABS(x) ((x)<0 ? -(x) : (x))
+#define REP(i,n) for(int i=0, _e(n); i<_e; i++)
+#define SET(t,v) memset((t), (v), sizeof(t))
+#define ALL(x) x.begin(), x.end()
+#define UNIQUE(c) (c).resize( unique( ALL(c) ) - (c).begin() )
+
+#define sz size()
+#define pb push_back
+
+typedef long long LL;
+typedef long double LD;
+typedef pair<int,int> pii;
+
+#ifdef DEBUG
+#define dbg(x) x
+#define dbgp(x) cerr << x ;
+#else
+#define dbg(x) //x
+#define dbgp(x) //cerr << x << endl;
+#endif
+
+
 int main(){
-    int t ;
-    cin >> t ;
+    int t ; 
+    cin >> t ; 
     while(t--){
-        ll n , x , y , d ;
-        cin >> n >> x >> y >> d ;
+        int n, s, e, d;
+        cin >> n >> s >> e >> d;
+        int direct = INT_MAX ; 
+        if ( abs(e-s)%d == 0 ) direct = abs(e-s)/d;
+        
+        int toStart = ceil( (s-1)/(double)d) ; 
+        int fromStart = INT_MAX ; 
+        if ( (e-1)%d == 0 ) fromStart = toStart + (e-1)/d ; 
 
-        ll step1 = 1e18 ;
+        int toEnd = ceil( (n-s)/(double)d ) ; 
+        int fromEnd = INT_MAX ; 
+        if ( (n-e) % d == 0 ) fromEnd = toEnd + (n-e) / d ; 
 
-        if ( abs(y-x) % d == 0  ) step1 = abs(y-x) / d ;
-
-        ll step2 = 1e18 ;
-
-        if ( (y-1)%d  == 0 ) step2 = ceil((x-1)/double(d)) + (y-1)/d ;
-
-        ll step3 = 1e18 ;
-
-        if ( (n-y)%d == 0 )step3 = ceil((n-x)/(double)d) + (n-y)/d;
-
-        ll ans = min(step1,step2);
-
-        ans = min(ans,step3) ;
-
-        if ( ans == 1e18 ) {
+        int result = min(direct,fromStart) ; 
+        result = min(result,fromEnd) ; 
+        if ( result == INT_MAX ){
             cout << -1 << endl;
         }
-        else cout << ans << endl;
+        else cout << result << endl;
 
     }
-
-    return 0 ;
+    
+    return 0;
 }
