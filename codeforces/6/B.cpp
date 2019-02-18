f5abbf0c15e61be65998c8bc804e3849d41f0d78
Amjad Alhouseini <amjadm2008@gmail.com>
2019-02-18 22:43:00 +0200

    Add solution for problem `B - President's Office` Link: https://codeforces.com/contest/6/problem/B Tags: implementation, *1100 Ref: https://codeforces.com/contest/6/submission/50139514

--- codeforces/6/B.cpp
+++ codeforces/6/B.cpp
@@ -1,32 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector<int> row = { 0 , 0 , -1 , 1 } ; 
-vector<int> col = { -1 , 1 , 0 , 0 } ; 
+char g[105][105];
+bool vis[105][105];
+int n , m ;
+char c ;
 int main(){
-    int n , m  ; 
-    char c ; 
-    cin >> n >> m >> c ; 
-    char g[n+2][m+2] ; 
+    cin >> n >> m >> c ;
     for(int i=0;i<=n+1;i++){
         for(int j=0;j<=m+1;j++){
-            g[i][j] = '.' ; 
+            vis[i][j] = false;
+            if ( i == 0 || i == n+1 || j == 0 || j == m+1 ){
+                g[i][j] = '.';
+            }
+            else{
+                cin >> g[i][j];
+            }
         }
     }
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=m;j++){
-            cin >> g[i][j] ; 
-        }
-    }
-    set<char> ans ; 
+    set<char> ss ;
     for(int i=1;i<=n;i++){
         for(int j=1;j<=m;j++){
             if ( g[i][j] == c ){
-                for(int k=0;k<4;k++){
-                    if ( g[i+row[k]][j+col[k]] != '.' && g[i+row[k]][j+col[k]] != c ) ans.insert(g[i+row[k]][j+col[k]]) ; 
+                if ( g[i-1][j] != c && g[i-1][j] != '.' && !vis[i-1][j] ){
+                    ss.insert(g[i-1][j]);
+                }
+                if ( g[i+1][j] != c && g[i+1][j] != '.' && !vis[i+1][j] ){
+                    ss.insert(g[i+1][j]);
+                }
+                if ( g[i][j+1] != c && g[i][j+1] != '.' && !vis[i][j+1] ){
+                    ss.insert(g[i][j+1]);
+                }
+                if ( g[i][j-1] != c && g[i][j-1] != '.' && !vis[i][j-1] ){
+                    ss.insert(g[i][j-1]);
                 }
             }
         }
     }
-    cout << ans.size() << endl;
-    return 0 ; 
+    cout << ss.size() << endl;
+
+
+    return 0 ;
 }
