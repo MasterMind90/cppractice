e1fdebd7b819f2ac995ba0eae313a92d3a1a1865
Amjad Alhouseini <amjadm2008@gmail.com>
2019-03-01 13:49:00 +0200

    Add solution for problem `B - Sum of Digits` Link: https://codeforces.com/contest/102/problem/B Tags: implementation, *1000 Ref: https://codeforces.com/contest/102/submission/50635972

--- codeforces/102/B.cpp
+++ codeforces/102/B.cpp
@@ -1,27 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
-int sum(string x){
-    int ans = 0 ; 
-    for(int i=0;i<x.size();i++){
-        ans += x[i] - '0';
+string sum(string a,string b){
+    int diff = abs((int)a.size()-(int)b.size());
+    if ( a.size() <= b.size() ) { 
+        for(int i=0;i<diff;i++){
+            a = "0" + a ;
+        }
     }
-    return ans;
-}
-string toString(int x){
-    stringstream ss ;
-    ss << x ; 
-    string s ; 
-    ss >> s ; 
-    return s ; 
+    else{
+        for(int i=0;i<diff;i++){
+            b = "0" + b ;
+        }
+    }
+    int c = 0 ; 
+    string ans = "" ;
+    for(int i=a.size()-1;i>=0;i--){
+        int x = a[i] - '0' ; 
+        int y = b[i] - '0' ; 
+        int z = x+y+c ; 
+        if ( z >= 10 ){
+            char cc = (z%10)+'0';
+            ans+=cc;
+            c = z/10;
+        }
+        else{
+            char cc = (z%10)+'0';
+            ans+=cc;
+            c = 0 ;
+        }
+    }
+    if ( c != 0 ){
+        ans += (c + '0') ; 
+    }
+    reverse(ans.begin(),ans.end());
+    return ans ; 
 }
 int main(){
     string s ;
     cin >> s ; 
     int cnt = 0 ;
-    while( s.size() > 1 ){
+    while(s.size()>1){
         cnt++;
-        int x = sum(s);
-        s = toString(x);
+        string tmp = "0" ; 
+        for(int i=0;i<s.size();i++){
+            tmp = sum(s.substr(i,1),tmp);
+        }
+        s = tmp ;
     }
     cout << cnt << endl;
     return 0;
