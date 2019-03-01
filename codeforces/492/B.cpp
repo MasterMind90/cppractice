a5e7b817e51d06da774245b25917feaefceaf364
Amjad Alhouseini <amjadm2008@gmail.com>
2019-03-01 16:01:00 +0200

    Add solution for problem `B - Vanya and Lanterns` Link: https://codeforces.com/contest/492/problem/B Tags: binary search, implementation, math, sortings, *1200 Ref: https://codeforces.com/contest/492/submission/50641218

--- codeforces/492/B.cpp
+++ codeforces/492/B.cpp
@@ -1,38 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n , m ;
+int n , l ; 
 vector<int> v ;
-bool p(double d){
-    double t = 0 ; 
-    for(int i=0;i<n;i++){
-        if ( v[i]-d <= t ) {
-            t = v[i]+d;
-            continue;
-        }
-        else return false;
-    }
-    if ( v[n-1] + d >= m ) return true ;
-    return false;
-}
 int main(){
-    cin >> n >> m ;
+    cin >> n >> l ;
     v.resize(n);
     for(int i=0;i<n;i++){
         cin >> v[i];
     }
     sort(v.begin(),v.end());
-    double l = 1e-9 ; 
-    double r = 1e10 ;
-    int x = 100 ;
-    while(x--){
-        double mid = l + (r-l)/2.0 ;
-        if ( p(mid) == true ){
-            r = mid ;
-        }
-        else {
-            l = mid ; 
-        }
+    int dist = 0 ; 
+    for(int i=1;i<n;i++){
+        dist = max(dist,v[i]-v[i-1]);
     }
-    cout << fixed << setprecision(9) << l << endl;
-    return 0;
+    double ans = dist / 2.0 ;
+    ans = max((double)v[0],ans);
+    ans = max(ans,(double)(l-v[v.size()-1]));
+    cout << fixed << setprecision(9) << ans << endl;
+    return 0 ; 
 }
