78acdbb6877965df9faaa928a36dcef736e9f3f9
Amjad Alhouseini <amjadm2008@gmail.com>
2019-03-25 17:01:00 +0200

    Add solution for problem `A - Game 23` Link: https://codeforces.com/contest/1141/problem/A Tags: implementation, math, *1000 Ref: https://codeforces.com/contest/1141/submission/51828459

--- codeforces/1141/A.cpp
+++ codeforces/1141/A.cpp
@@ -1,68 +1,30 @@
-#ifndef LOCAL
-#pragma GCC optimize("Ofast")
-#pragma GCC target("avx,avx2,fma")
-#pragma GCC optimization ("unroll-loops")
-#endif
-#include "bits/stdc++.h"
-#include <ext/pb_ds/assoc_container.hpp> // Common file
-#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
-#include <ext/pb_ds/detail/standard_policies.hpp>
+#include <bits/stdc++.h>
 using namespace std;
-using namespace __gnu_pbds;
-#define sim template < class c
-#define int long long
-#define ris return * this
-#define dor > debug & operator <<
-#define eni(x) sim > typename \
-  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
-sim > struct rge { c b, e; };
-sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
-sim > auto dud(c* x) -> decltype(cerr << *x, 0);
-sim > char dud(...);
-struct debug {
-#ifdef LOCAL
-~debug() { cerr << '\n'; }
-eni(!=) cerr << boolalpha << i; ris; }
-eni(==) ris << range(begin(i), end(i)); }
-sim, class b dor(pair < b, c > d) {
-  ris << "(" << d.first << ", " << d.second << ")";
-}
-sim dor(rge<c> d) {
-  *this << "[";
-  for (auto it = d.b; it != d.e; ++it)
-    *this << ", " + 2 * (it == d.b) << *it;
-  ris << "]";
-}
-#else
-sim dor(const c&) { ris; }
-#endif
-};
-#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
-#define fastio ios_base::sync_with_stdio(false);cin.tie(0);
-typedef tree< pair<int,int>, null_type, less<pair<int,int> >, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
-typedef long long ll;
-const ll MOD = 1e9 + 7 ;
-const ll N = 2e5 + 10 ;
-const ll INF = 1e18 + 10 ;
-int a , b ;
-int dfs(int x){
-    if ( x == b ) {
-        return 0 ;
+
+int n , m ;
+
+bool dfs(int n,int cnt){
+    if ( n == m ){
+        cout << cnt << endl;
+        return true ;
     }
-    int ans = INF ;
-    if ( x * 2 <= b ) {
-        ans = min(ans, 1 + dfs(x * 2)) ;
+    if ( n > m ) {
+        return false ;
     }
-    if ( x * 3 <= b ) {
-        ans = min(ans, 1 + dfs(x * 3)) ;
+
+    if ( dfs(n*2,cnt+1) ){
+        return true ;
     }
-    return ans ;
+    if ( dfs(n*3,cnt+1) ){
+        return true ;
+    }
+    return false ;
 }
-signed main(){
-    fastio
-    cin >> a >> b ;
-    int ans = dfs(a) ;
-    if ( ans == INF ) cout << -1 << endl;
-    else cout << ans << endl;
-    return 0; 
+
+int main(){
+    cin >> n >> m ;
+    if ( !dfs(n,0) ){
+        cout << -1 << endl;
+    }
+    return 0 ;
 }
