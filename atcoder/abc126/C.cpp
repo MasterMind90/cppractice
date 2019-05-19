8995dde4434b552333baeac1c33f696a0e629c26
Amjad Alhouseini <amjadm2008@gmail.com>
2019-05-19 18:41:00 +0300

    Add solution for problem `C - Dice and Coin` Link: https://atcoder.jp/contests/abc126/tasks/abc126_c Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/abc126/submissions/5483127

---
+++ atcoder/abc126/C.cpp
@@ -0,0 +1,51 @@
+#ifndef LOCAL
+#pragma GCC optimize("O3")
+#endif
+#include "bits/stdc++.h"
+using namespace std;
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+typedef long long ll;
+
+int main(){
+	int n , k ; 
+	cin >> n >> k ; 
+	long double ans = 0 ; 
+	for(int i=1;i<=n;i++){
+		int start = i ; 
+		int den = 1 ; 
+		while(start<k){
+			start*=2 ; 
+			den*=2 ; 
+		}
+		ans += (1.0/n) * (1.0/den) ; 
+	}
+	cout << fixed << setprecision(12) << ans << endl;
+    return 0 ;
+}
