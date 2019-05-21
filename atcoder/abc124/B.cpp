487482c3ffaaf81cd7f319eac3f0f92e969054fa
Amjad Alhouseini <amjadm2008@gmail.com>
2019-05-21 12:29:00 +0300

    Add solution for problem `B - Great Ocean View` Link: https://atcoder.jp/contests/abc124/tasks/abc124_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc124/submissions/5501934

--- atcoder/abc124/B.cpp
+++ atcoder/abc124/B.cpp
@@ -1,24 +1,23 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-int main (){
 
-   int N;
-cin>>N;
-int H[N];
-for(int i=0;i<N;i++)
-    cin>>H[i];
-int cnt=1;
-int nax = H[0] ;
-for(int i=1;i<N;i++){
-    if ( H[i] >= nax ){
-        cnt++;
-        nax = H[i] ;
+int main(){
+    int n ;
+    cin >> n ;
+    vector<int> v(n) ;
+    for(int &x : v){
+        cin >> x ;
     }
-}
-cout<<cnt;
-
-
-
-    return 0;
+    vector<int> sum(n+1,0);
+    for(int i=1;i<=n;i++){
+        sum[i] = max(sum[i-1],v[i-1]);
+    }
+    int cnt = 0 ;
+    for(int i=1;i<=n;i++){
+        if ( v[i-1] == sum[i] ){
+            cnt++;
+        }
+    }
+    cout << cnt << endl;
+    return 0 ;
 }
