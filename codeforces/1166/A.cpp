e91a9a0aa05d446256adddb0c41c238004913bfb
Amjad Alhouseini <amjadm2008@gmail.com>
2019-05-22 12:23:00 +0300

    Add solution for problem `A - Silent Classroom` Link: https://codeforces.com/contest/1166/problem/A Tags: combinatorics, greedy, *900 Ref: https://codeforces.com/contest/1166/submission/54477732

---
+++ codeforces/1166/A.cpp
@@ -0,0 +1,56 @@
+#ifndef LOCAL
+#pragma GCC optimize("O3")
+#endif
+#include "bits/stdc++.h"
+using namespace std;
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+typedef long long ll;
+int main(){
+	int n ;
+	cin >> n ;
+	int cnt[26] = {0} ;
+	for(int i=0;i<n;i++){
+		string x ;
+		cin >> x ;
+		cnt[x[0]-'a']++;
+	}
+	int ans = 0 ;
+	for(int i=0;i<26;i++){
+		if ( cnt[i] > 2){
+            int first = cnt[i]/2 + cnt[i]%2 ;
+            int second = cnt[i]/2 ;
+            debug() << imie(first) imie(second) ;
+            ans += first*(first-1)/2;
+            ans += second*(second-1)/2;
+		}
+	}
+	cout << ans << endl;
+	return 0 ;
+}
