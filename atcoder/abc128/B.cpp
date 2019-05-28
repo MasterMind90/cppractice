b4434735b7738be4fa02700ccea5bc882eeb1f88
Amjad Alhouseini <amjadm2008@gmail.com>
2019-05-28 18:49:00 +0300

    Add solution for problem `B - Guidebook` Link: https://atcoder.jp/contests/abc128/tasks/abc128_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc128/submissions/5679212

--- atcoder/abc128/B.cpp
+++ atcoder/abc128/B.cpp
@@ -1,36 +1,54 @@
-#include <bits/stdc++.h>
-
+#ifndef LOCAL
+#pragma GCC optimize("O3")
+#endif
+#include "bits/stdc++.h"
 using namespace std;
-#define int long long
-
-bool comp(pair <string,pair<int,int>> a, pair<string,pair<int,int>> b){
-	if (a.first < b.first)
-		return true;
-	else if (a.first > b.first)
-		return false;
-	else {
-		if (a.second.first > b.second.first)
-			return true;
-
-		else return false;
-	}
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
 }
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+typedef long long ll;
 
-signed main(){
-	
-	int n;
-	cin >> n;
-
-	vector <pair <string,pair< int , int >>> city(n);
-	for (int i = 0; i < n; i++){
-		cin >> city[i].first >> city[i].second.first;
-		city[i].second.second = i+1;
+int main(){
+	int n ; 
+	cin >> n ; 
+	vector<pair<string,pair<int,int> > > v ; 
+	for(int i=0;i<n;i++){
+		string x; 
+		cin >> x; 
+		int s ; 
+		cin >> s ;
+		v.push_back({x,{-s,i+1}}) ; 
 	}
-
-	sort (city.begin(),city.end(),comp);
-
-	for (int i = 0; i < n; i++){
-		cout << city[i].second.second << endl;
+	debug() << imie(v) ; 
+	sort(v.begin(),v.end());
+	debug() << imie(v) ; 
+	for(int i=0;i<n;i++){
+		cout << v[i].second.second << endl;
 	}
-	return 0 ;
+    return 0 ;
 }
