999f320b2354ff4c24f766713467a7ed27364dac
Amjad Alhouseini <amjadm2008@gmail.com>
2019-06-06 15:24:00 +0300

    Add solution for problem `C - Vacation` Link: https://atcoder.jp/contests/dp/tasks/dp_c Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/5806823

--- atcoder/dp/C.cpp
+++ atcoder/dp/C.cpp
@@ -2,8 +2,6 @@
 #pragma GCC optimize("O3")
 #endif
 #include "bits/stdc++.h"
-#include <ext/pb_ds/assoc_container.hpp>
-using namespace __gnu_pbds;
 using namespace std;
 #define sim template < class c
 #define ris return * this
@@ -33,38 +31,35 @@ sim dor(const c&) { ris; }
 #endif
 };
 #define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
-#define fastio ios_base::sync_with_stdio(false);cin.tie(0);
 typedef long long ll;
-typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> indexed_set; 
-const int MAXN = 1e5 + 10 ; 
-int n ;
-vector<int> A(MAXN), B(MAXN), C(MAXN) ; 
-int dp[MAXN][4] ; 
-int dfs(int idx,int last){
-	if ( idx == n ){
-		return 0 ; 
+const ll MAXN = 1e5 + 10 ; 
+ll n ; 
+vector<vector<ll> > activity ; 
+ll dp[MAXN][3] ; 
+ll dfs(ll day,ll act){
+	if ( day == n-1 ){
+		return activity[day][act] ; 
 	}
-	int &res = dp[idx][last] ; 
-	if ( res != -1 ) return res ; 
-	int ans = 0 ; 
-	if ( last != 1 ){
-		ans = max(ans,A[idx] + dfs(idx+1,1) );  
+	if ( dp[day][act] ) return dp[day][act] ; 
+	ll val = 0 ; 
+	for(ll i=0;i<3;i++){
+		if ( i != act ){
+			val = max(val, activity[day][act] + dfs(day+1,i) ) ; 
+		}
 	}
-	if ( last != 2 ){
-		ans = max(ans,B[idx] + dfs(idx+1,2) );  
-	}
-	if ( last != 3 ){
-		ans = max(ans,C[idx] + dfs(idx+1,3) );  
-	}
-	return res = ans ; 
+	dp[day][act] = val  ; 
+	return val ; 
 }
 int main(){
-    fastio
-    memset(dp,-1,sizeof dp) ; 
-    cin >> n ; 
-    for(int i=0;i<n;i++){
-    	cin >> A[i] >> B[i] >> C[i] ; 
-    }
-    cout << dfs(0,0) << endl;
-    return 0 ;
+	cin >> n ; 
+	activity.resize(n) ; 
+	for(ll i=0;i<n;i++){
+		for(ll j=0;j<3;j++){
+			ll x ;
+			cin >> x ; 
+			activity[i].push_back(x) ; 
+		}
+	}
+	cout << max({dfs(0,0),dfs(0,1),dfs(0,2)}) << endl;
+	return 0 ;
 }
