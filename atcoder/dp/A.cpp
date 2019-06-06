e9b2ed036b8f1cc9fd1576cbbb1bdb66c6ed1fae
Amjad Alhouseini <amjadm2008@gmail.com>
2019-06-06 14:54:00 +0300

    Add solution for problem `A - Frog 1` Link: https://atcoder.jp/contests/dp/tasks/dp_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/5806546

--- atcoder/dp/A.cpp
+++ atcoder/dp/A.cpp
@@ -2,8 +2,6 @@
 #pragma GCC optimize("O3")
 #endif
 #include "bits/stdc++.h"
-#include <ext/pb_ds/assoc_container.hpp>
-using namespace __gnu_pbds;
 using namespace std;
 #define sim template < class c
 #define ris return * this
@@ -33,27 +31,29 @@ sim dor(const c&) { ris; }
 #endif
 };
 #define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
-#define fastio ios_base::sync_with_stdio(false);cin.tie(0);
 typedef long long ll;
-typedef tree<ll,null_type,less<ll>,rb_tree_tag,tree_order_statistics_node_update> indexed_set; 
-const ll INF = 1e18 + 10 ; 
+int n ; 
+vector<ll> v ; 
+vector<ll> dp ; 
+ll dfs(int x){
+	if ( x == n-1 )
+		return 0 ; 
+	if ( dp[x] != 1e18 ) return dp[x] ; 
+	ll first = abs(v[x] - v[x+1]) + dfs(x+1) ; 
+	ll second = 1e18 ; 
+	if ( x + 2 < n )
+		second = abs(v[x]-v[x+2]) + dfs(x+2) ; 
+	dp[x] = min(first,second) ; 
+	return dp[x] ; 
+}
+
 int main(){
-    fastio
-    ll n ;
-    cin >> n ; 
-    vector<ll> v(n+1) ; 
-    for(ll i=1;i<=n;i++){
-    	cin >> v[i] ; 
-    }
-    ll dp[n+1] ; // the minimum cost up the current stone
-    for(ll i=0;i<=n;i++){
-    	dp[i] = INF ; 
-    }
-    dp[0] = dp[1] = 0 ; 
-    dp[2] = abs(v[2]-v[1]) ;
-    for(ll i=3;i<=n;i++){
-    	dp[i] = min(abs(v[i]-v[i-1])+dp[i-1],abs(v[i]-v[i-2])+dp[i-2]);
-    }
-    cout << dp[n] << endl;
+	cin >> n ; 
+	v.resize(n) ; 
+	dp.resize(n,1e18) ; 
+	for(ll &x : v){
+		cin >> x ; 
+	}
+	cout << dfs(0) << endl;
     return 0 ;
 }
