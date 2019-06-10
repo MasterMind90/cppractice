0ded1f80df22977ba1da6b571739c2880161bfd5
Amjad Alhouseini <amjadm2008@gmail.com>
2019-06-10 12:40:00 +0300

    Add solution for problem `D - Knapsack 1` Link: https://atcoder.jp/contests/dp/tasks/dp_d Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/5868115

--- atcoder/dp/D.cpp
+++ atcoder/dp/D.cpp
@@ -1,34 +1,64 @@
-#include <bits/stdc++.h>
+#ifndef LOCAL
+#pragma GCC optimize("O3")
+#endif
+#include "bits/stdc++.h"
 using namespace std;
-const long long MAXN = 1e5 + 10 ;
-long long n , W ;
-vector<long long> v , w ;
-long long dp[110][MAXN] ;
-long long dfs(long long x,long long curr){
-    if ( x == n ){
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+typedef long long ll;
+const ll MAXW = 1e5 + 10 ;
+ll n , W ;
+ll w[110] , v[110] ;
+ll dp[110][MAXW] ;
+ll dfs(ll x,ll currWeight){
+    if ( x == n ) {
         return 0 ;
     }
-    if ( dp[x][curr] != -1 ) return dp[x][curr] ;
-    long long choice1 = 0 + dfs(x+1,curr) ;
-    long long choice2 = -1e18 ;
-    if ( curr + w[x] <= W ){
-        choice2 = v[x] + dfs(x+1,curr+w[x]) ;
+    if ( dp[x][currWeight] ) return dp[x][currWeight] ;
+    ll choice1 = 0 ;
+    ll choice2 = 0 ;
+    if ( currWeight < w[x] ){
+        choice1 = dfs(x+1,currWeight) ;
     }
-    long long ans = max(choice1,choice2);
-    dp[x][curr] = ans ;
-    return ans ;
+    else{
+        choice1 = dfs(x+1,currWeight) ;
+        choice2 = v[x] + dfs(x+1,currWeight-w[x]) ;
+    }
+    dp[x][currWeight] = max(choice1,choice2) ;
+    return dp[x][currWeight] ;
 }
 
-
 int main(){
-    memset(dp,-1,sizeof dp);
     cin >> n >> W ;
-    v.resize(n) ;
-    w.resize(n) ;
     for(int i=0;i<n;i++){
         cin >> w[i] >> v[i] ;
     }
-    cout << dfs(0,0) << endl;
+    cout << dfs(0,W) << endl;
     return 0 ;
 }
-
