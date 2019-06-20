a35a634656cfc62e15969de4bb44526e565e180c
Amjad Alhouseini <amjadm2008@gmail.com>
2019-06-20 05:50:00 +0300

    Add solution for problem `C2 - Exam in BerSU (hard version)` Link: https://codeforces.com/contest/1185/problem/C2 Tags: brute force, data structures, greedy, math, *1700 Ref: https://codeforces.com/contest/1185/submission/55809816

--- codeforces/1185/C2.cpp
+++ codeforces/1185/C2.cpp
@@ -7,63 +7,79 @@ using namespace std;
 #define ris return * this
 #define dor > debug & operator <<
 #define eni(x) sim > typename \
-  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
-sim > struct rge { c b, e; };
-sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
-sim > auto dud(c* x) -> decltype(cerr << *x, 0);
-sim > char dud(...);
-struct debug {
+enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+	sim > struct rge { c b, e; };
+	sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+	sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+	sim > char dud(...);
+	struct debug {
 #ifdef LOCAL
-~debug() { cerr << endl; }
-eni(!=) cerr << boolalpha << i; ris; }
-eni(==) ris << range(begin(i), end(i)); }
-sim, class b dor(pair < b, c > d) {
-  ris << "(" << d.first << ", " << d.second << ")";
-}
-sim dor(rge<c> d) {
-  *this << "[";
-  for (auto it = d.b; it != d.e; ++it)
-    *this << ", " + 2 * (it == d.b) << *it;
-  ris << "]";
-}
+		~debug() { cerr << endl; }
+		eni(!=) cerr << boolalpha << i; ris; }
+		eni(==) ris << range(begin(i), end(i)); }
+		sim, class b dor(pair < b, c > d) {
+			ris << "(" << d.first << ", " << d.second << ")";
+		}
+		sim dor(rge<c> d) {
+			*this << "[";
+			for (auto it = d.b; it != d.e; ++it)
+				*this << ", " + 2 * (it == d.b) << *it;
+			ris << "]";
+		}
 #else
-sim dor(const c&) { ris; }
+		sim dor(const c&) { ris; }
 #endif
-};
+	};
 #define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
-using ll = long long ;
+	typedef long long ll;
 
-int main(){
-  int n , m ;
-  cin >> n >> m ;
-  ll sum = 0 ;
-  vector<int> cnt(110,0) ;
-  for(int i=0;i<n;i++){
-    int x ;
-    cin >> x ;
-    sum += x ;
-    if ( sum <= m ){
-      cout << 0 << ' ' ;
-      cnt[x]++ ;
-      continue ;
-    }
-    int res = 0 ;
-    int cur = sum ;
-    for(int j=100;j;j--){
-      if ( cnt[j] ){
-        int c = (cur - m + j - 1)/j ;
-        c = min(c,cnt[j]) ;
-        res += c ;
-        if ( cur - j*c <= m ){
-          cout << res << ' ' ;
-          break;
-        }
-        cur -= c*j ;
-      }
-    }
-    cnt[x]++ ;
-  }
-  return 0 ;
-}
-
-
+	int main(){
+		ll n , m ; 
+		cin >> n >> m ; 
+		vector<ll> t(n) ; 
+		vector<ll> sum(n+1) ; 
+		vector<ll> cnt(110) ; 
+		sum[0] = 0 ; 
+		for(ll i=0;i<n;i++){
+			cin >> t[i] ; 
+			sum[i+1] = sum[i] + t[i] ; 
+			debug() << imie(sum) ; 
+			if ( sum[i+1] <= m ){
+				cout << 0 << ' ' ;
+			}
+			else{
+				ll cur = sum[i+1] ; 
+				debug() << imie(cur);
+				ll v = t[i]; 
+				debug() << imie(v) ; 
+				ll res = 0 ; 
+				for(ll j=100;j>=1;j--){
+					if ( cnt[j] ){
+						debug() << imie(j) imie(cnt[j]);
+						ll L = 1 , R = cnt[j] ; 
+						ll ans = -1 ; 
+						while(L<=R){
+							ll mid = L + (R-L) / 2 ; 
+							if ( cur-mid*j  <= m ){
+								debug() << imie(mid) ; 
+								ans = mid ; 
+								R = mid - 1 ; 
+							}
+							else L = mid + 1 ;
+						}
+						if ( ans == -1 ) {
+							cur -= cnt[j]*j;
+							res += cnt[j] ; 
+						}
+						else{
+							res += ans ; 
+							cout << res << ' ' ; 
+							break;
+						}
+					}
+				}
+			}
+			cnt[t[i]]++;
+		}
+		return 0 ;
+	}
