c1f4a5fb1e4a8f9f4a9eb90c4e8192c54c6b5041
Amjad Alhouseini <amjadm2008@gmail.com>
2019-06-20 05:26:00 +0300

    Add solution for problem `C2 - Exam in BerSU (hard version)` Link: https://codeforces.com/contest/1185/problem/C2 Tags: brute force, data structures, greedy, math, *1700 Ref: https://codeforces.com/contest/1185/submission/55809356

--- codeforces/1185/C2.cpp
+++ codeforces/1185/C2.cpp
@@ -7,79 +7,74 @@ using namespace std;
 #define ris return * this
 #define dor > debug & operator <<
 #define eni(x) sim > typename \
-enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
-	sim > struct rge { c b, e; };
-	sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
-	sim > auto dud(c* x) -> decltype(cerr << *x, 0);
-	sim > char dud(...);
-	struct debug {
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
 #ifdef LOCAL
-		~debug() { cerr << endl; }
-		eni(!=) cerr << boolalpha << i; ris; }
-		eni(==) ris << range(begin(i), end(i)); }
-		sim, class b dor(pair < b, c > d) {
-			ris << "(" << d.first << ", " << d.second << ")";
-		}
-		sim dor(rge<c> d) {
-			*this << "[";
-			for (auto it = d.b; it != d.e; ++it)
-				*this << ", " + 2 * (it == d.b) << *it;
-			ris << "]";
-		}
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
 #else
-		sim dor(const c&) { ris; }
+sim dor(const c&) { ris; }
 #endif
-	};
+};
 #define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
-	typedef long long ll;
-
-	int main(){
-		ll n , m ; 
-		cin >> n >> m ; 
-		vector<ll> t(n) ; 
-		vector<ll> sum(n+1) ; 
-		vector<ll> cnt(110) ; 
-		sum[0] = 0 ; 
-		for(ll i=0;i<n;i++){
-			cin >> t[i] ; 
-			sum[i+1] = sum[i] + t[i] ; 
-			debug() << imie(sum) ; 
-			if ( sum[i+1] <= m ){
-				cout << 0 << ' ' ;
-			}
-			else{
-				ll cur = sum[i+1] ; 
-				debug() << imie(cur);
-				ll v = t[i]; 
-				debug() << imie(v) ; 
-				ll res = 0 ; 
-				for(ll j=100;j>=1;j--){
-					if ( cnt[j] ){
-						debug() << imie(j) imie(cnt[j]);
-						ll L = 1 , R = cnt[j] ; 
-						ll ans = -1 ; 
-						while(L<=R){
-							ll mid = L + (R-L) / 2 ; 
-							if ( cur-mid*j  <= m ){
-								debug() << imie(mid) ; 
-								ans = mid ; 
-								R = mid - 1 ; 
-							}
-							else L = mid + 1 ;
-						}
-						if ( ans == -1 ) {
-							cur -= cnt[j]*j;
-							res += cnt[j] ; 
-						}
-						else{
-							res += ans ; 
-							cout << res << ' ' ; 
-							break;
-						}
-					}
-				}
-			}
-			cnt[t[i]]++;
-		}
-		return 0 ;
-	}
+#define fastio ios_base::sync_with_stdio(false);cin.tie(0);
+typedef long long ll;
+const int MAXT = 110 ; 
+int main(){
+    fastio
+    int n , m ; 
+    cin >> n >> m ; 
+    vector<int> cnt(MAXT,0);
+    int sum = 0 ; 
+    for(int i=0;i<n;i++){
+    	int x ; 
+    	cin >> x ; 
+    	sum += x ; 
+    	if ( sum <= m ){
+    		cout << 0 << ' ' ; 
+    	}
+    	else{
+    		int cur = sum ; 
+    		int res = 0 ; 
+    		for(int j=100;j>=1;j--){
+    			if ( cnt[j] ){
+    				int L = 1 , R = cnt[j] ; 
+    				int ans = -1 ; 
+    				while(L<=R){
+    					int mid = L + (R-L) / 2 ;
+    					if ( cur-m <= mid*j ){
+    						ans = mid ; 
+    						R = mid - 1; 
+    					}
+    					else L = mid + 1 ; 
+    				}
+    				if ( ans == -1 ){
+    					cur -= j*cnt[j];
+    					res += cnt[j] ; 
+    				}
+    				else{
+    					res += ans ; 
+    					cout << res << ' ' ; 
+    					break;
+    				}
+    			}
+    		}
+    	}
+    	cnt[x]++;
+    }
+    return 0 ;
+}
