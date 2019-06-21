1a639bfb3d7316363527d5d87608851e0ab97858
Amjad Alhouseini <amjadm2008@gmail.com>
2019-06-21 20:38:00 +0300

    Add solution for problem `B - Nick and Array` Link: https://codeforces.com/contest/1180/problem/B Tags: greedy, implementation, *1500 Ref: https://codeforces.com/contest/1180/submission/55885285

---
+++ codeforces/1180/B.cpp
@@ -0,0 +1,90 @@
+#ifndef LOCAL
+#pragma GCC optimize("O3")
+#endif
+#include "bits/stdc++.h"
+using namespace std;
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+#define fastio ios_base::sync_with_stdio(false);cin.tie(0);
+typedef long long ll;
+
+int main(){
+    fastio
+    int n ; 
+    cin >> n ; 
+    vector<int> v(n) ; 
+    int neg = 0 ; 
+    bool one = false ; 
+    int oneIndex = -1 ; 
+    for(int i=0;i<n;i++){
+    	cin >> v[i] ; 
+    	if ( v[i] >= 0 ){
+    		v[i] = -(v[i]+1) ; 
+    	}
+    	if ( v[i] == -1 ) {
+    		one = true; 
+    		oneIndex = i ; 
+    	}
+    	neg++ ; 
+    }
+    debug() << imie(v) ; 
+    if ( neg % 2 == 0 ){
+    	for(int x : v){
+    		cout << x << ' ' ; 
+    	}
+    	cout << endl;
+    	return 0 ; 
+    }
+    int nax = -1e9 ; 
+    int naxIndex = -1 ; 
+    for(int i=0;i<n;i++){
+    	if ( abs(v[i]) == 1 ) continue ; 
+    	// do not forget may be all ones 
+    	if ( nax < abs(v[i]) ){
+    		nax = abs(v[i]) ; 
+    		naxIndex = i ; 
+    	}
+    }
+    if ( naxIndex != -1 ){
+    	v[naxIndex] = -v[naxIndex] - 1 ; 
+    	for(int i=0;i<n;i++){
+    		cout << v[i] << ' ' ; 
+    	}
+    	cout << endl;
+    	return 0 ; 
+    }
+    if ( one ){
+    	v[oneIndex] = -v[oneIndex] - 1 ; 
+    }
+    for(int i=0;i<n;i++){
+    	cout << v[i] << ' ' ; 
+    }
+    cout << endl;
+    return 0 ;
+}
