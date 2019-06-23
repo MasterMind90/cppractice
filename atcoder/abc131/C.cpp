c8205e2b391496f535015a15ae8da14f7cbb1b9b
Amjad Alhouseini <amjadm2008@gmail.com>
2019-06-23 11:35:00 +0300

    Add solution for problem `C - Anti-Division` Link: https://atcoder.jp/contests/abc131/tasks/abc131_c Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/abc131/submissions/6094378

---
+++ atcoder/abc131/C.cpp
@@ -0,0 +1,48 @@
+#ifndef LOCAL
+#pragma GCC optimize("O3")
+#endif
+#include "bits/stdc++.h"
+using namespace std;
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+using ll = long long ;
+
+int main(){
+    ll a , b , c , d ; 
+    cin >> a >> b >> c >> d ; 
+    ll first = b/c - (a-1)/c ; 
+    ll second = b/d - (a-1)/d ; 
+    ll lcm = c*d / __gcd(c,d) ; 
+    ll third = b / lcm - (a-1) / lcm ; 
+    ll ans = (b-a+1) - (first + second - third) ; 
+    cout << ans << endl;
+    return 0 ; 
+}
+
+
