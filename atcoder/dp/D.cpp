ef752cba5601f4064e8e12d18501accb5e9c1096
Amjad Alhouseini <amjadm2008@gmail.com>
2019-06-26 12:17:00 +0300

    Add solution for problem `D - Knapsack 1` Link: https://atcoder.jp/contests/dp/tasks/dp_d Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/6127567

--- atcoder/dp/D.cpp
+++ atcoder/dp/D.cpp
@@ -1,30 +1,34 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-const long long INF = 1e18 + 10 ;
+const long long MAXN = 1e5 + 10 ;
 long long n , W ;
-vector<long long> w , v ;
-long long dp[110][100010] ;
-long long dfs(long long x, long long cur){
-    if ( cur > W ) return -INF ;
+vector<long long> v , w ;
+long long dp[110][MAXN] ;
+long long dfs(long long x,long long curr){
     if ( x == n ){
         return 0 ;
     }
-    if ( dp[x][cur] != -1 ) return dp[x][cur] ;
-    long long choice1 = dfs(x + 1, cur) ;
-    long long choice2 = v[x] + dfs(x + 1, cur + w[x]);
-    return dp[x][cur] = max(choice1, choice2) ;
+    if ( dp[x][curr] != -1 ) return dp[x][curr] ;
+    long long choice1 = 0 + dfs(x+1,curr) ;
+    long long choice2 = -1e18 ;
+    if ( curr + w[x] <= W ){
+        choice2 = v[x] + dfs(x+1,curr+w[x]) ;
+    }
+    long long ans = max(choice1,choice2);
+    dp[x][curr] = ans ;
+    return ans ;
 }
+
 
 int main(){
-    memset(dp, -1, sizeof dp) ;
+    memset(dp,-1,sizeof dp);
     cin >> n >> W ;
-    w.resize(n) ;
     v.resize(n) ;
-    for(int i = 0; i < n; i++){
+    w.resize(n) ;
+    for(int i=0;i<n;i++){
         cin >> w[i] >> v[i] ;
     }
-    cout << dfs(0, 0) << endl;
-
+    cout << dfs(0,0) << endl;
     return 0 ;
 }
+
