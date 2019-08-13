3dfb6d7e76d0bd1f05939b6731638c932a3e8e4a
Amjad Alhouseini <amjadm2008@gmail.com>
2019-08-13 18:08:00 +0300

    Add solution for problem `C - Common Divisors` Link: https://codeforces.com/contest/1203/problem/C Tags: implementation, math, *1300 Ref: https://codeforces.com/contest/1203/submission/58730241

--- codeforces/1203/C.cpp
+++ codeforces/1203/C.cpp
@@ -1,34 +1,60 @@
-//CodeF Common Divisors 1203C
-#include<iostream>
-#include<algorithm>
-#include<vector>
-#include<cmath>
- 
-#define ll long long
- 
+#ifndef LOCAL
+#pragma GCC optimize("O3")
+#endif
+#include "bits/stdc++.h"
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
 using namespace std;
- 
- 
-int main()
-{
- cin.tie(0);ios_base::sync_with_stdio(0);
- 
- int n;
- cin>>n;
- ll g=0;
- while(n--){
-     ll k;
-     cin>>k;
-     g=__gcd(g,k);
- }
- ll ans=0;
- for(int i=1;i*1ll*i<=g;i++){
-     if(g%i==0){
-         ans++;
-         if((i*1ll*i)!=g){ans++;}
-     }
- }
- cout<<ans<<"\n";
- 
- return 0;
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+#define fastio ios_base::sync_with_stdio(false);cin.tie(0);
+typedef long long ll;
+typedef tree<ll,null_type,less<ll>,rb_tree_tag,tree_order_statistics_node_update> indexed_set; 
+
+int main(){
+    fastio
+    ll n ;
+    cin >> n ;
+    ll x; 
+    cin >> x ;
+    ll gcd = x ; 
+    for(ll i=1;i<n;i++){
+    	cin >> x ;
+    	gcd = __gcd(gcd,x) ; 
+    }
+    ll cnt = 0 ; 
+    for(ll i=1;i*i<=gcd;i++){
+    	if ( gcd % i == 0 ){
+    		if ( gcd / i == i ) cnt++ ; 
+    		else cnt += 2 ; 
+    	}
+    }
+    cout << cnt << endl;
+    return 0 ;
 }
