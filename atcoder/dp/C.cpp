23787d4c1f9e95d95588710e3c19d5cd75b7c5bc
Amjad Alhouseini <amjadm2008@gmail.com>
2019-08-17 14:45:00 +0300

    Add solution for problem `C - Vacation` Link: https://atcoder.jp/contests/dp/tasks/dp_c Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/6954125

--- atcoder/dp/C.cpp
+++ atcoder/dp/C.cpp
@@ -1,16 +1,11 @@
 #ifndef LOCAL
-#pragma GCC optimize("Ofast")
-#pragma GCC target("avx,avx2,fma")
-#pragma GCC optimization ("unroll-loops")
+#pragma GCC optimize("O3")
 #endif
 #include "bits/stdc++.h"
-#include <ext/pb_ds/assoc_container.hpp> // Common file
-#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
-#include <ext/pb_ds/detail/standard_policies.hpp>
-using namespace std;
+#include <ext/pb_ds/assoc_container.hpp>
 using namespace __gnu_pbds;
+using namespace std;
 #define sim template < class c
-#define int long long
 #define ris return * this
 #define dor > debug & operator <<
 #define eni(x) sim > typename \
@@ -21,7 +16,7 @@ sim > auto dud(c* x) -> decltype(cerr << *x, 0);
 sim > char dud(...);
 struct debug {
 #ifdef LOCAL
-~debug() { cerr << '\n'; }
+~debug() { cerr << endl; }
 eni(!=) cerr << boolalpha << i; ris; }
 eni(==) ris << range(begin(i), end(i)); }
 sim, class b dor(pair < b, c > d) {
@@ -39,50 +34,37 @@ sim dor(const c&) { ris; }
 };
 #define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
 #define fastio ios_base::sync_with_stdio(false);cin.tie(0);
-typedef tree< pair<int,int>, null_type, less<pair<int,int> >, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
 typedef long long ll;
-const ll MOD = 1e9 + 7 ;
-const ll N = 1e6 + 10 ;
-const ll INF = 1e18 + 10 ;
-int n;
-vector<vector<int> > v(100000 , vector<int> (3));
-int dp[100000 + 1][4];
-int dfs(int i , int last) {
-    if(i == n) {
-        return 0;
-    }
-    if(dp[i][last] != -1) return dp[i][last] ;
-    int ans = 0;
-
-    if(last == 0) {
-        ans = max(ans , dfs(i + 1 , 1) + v[i][0]);
-        ans = max(ans , dfs(i + 1 , 2) + v[i][1]);
-        ans = max(ans , dfs(i + 1 , 3) + v[i][2]);
-    }
-    else if(last == 1){
-        ans = max(ans , dfs(i + 1 , 2) + v[i][1]);
-        ans = max(ans , dfs(i + 1 , 3) + v[i][2]);
-    }
-    else if ( last == 2 ) {
-        ans = max(ans , dfs(i + 1 , 1) + v[i][0]);
-        ans = max(ans , dfs(i + 1 , 3) + v[i][2]);
-    }
-    else if ( last == 3 ) {
-        ans = max(ans , dfs(i + 1 , 1) + v[i][0]);
-        ans = max(ans , dfs(i + 1 , 2) + v[i][1]);
-    }
-    return dp[i][last] = ans ; 
+typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> indexed_set; 
+const int MAXN = 1e5 + 10 ; 
+int n ;
+vector<int> A(MAXN), B(MAXN), C(MAXN) ; 
+int dp[MAXN][4] ; 
+int dfs(int idx,int last){
+	if ( idx == n ){
+		return 0 ; 
+	}
+	int &res = dp[idx][last] ; 
+	if ( res != -1 ) return res ; 
+	int ans = 0 ; 
+	if ( last != 1 ){
+		ans = max(ans,A[idx] + dfs(idx+1,1) );  
+	}
+	if ( last != 2 ){
+		ans = max(ans,B[idx] + dfs(idx+1,2) );  
+	}
+	if ( last != 3 ){
+		ans = max(ans,C[idx] + dfs(idx+1,3) );  
+	}
+	return res = ans ; 
 }
-signed main(){
+int main(){
     fastio
-    memset(dp , -1 , sizeof dp);
-    cin >> n;
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < 3; j++) {
-            cin >> v[i][j];
-        }
+    memset(dp,-1,sizeof dp) ; 
+    cin >> n ; 
+    for(int i=0;i<n;i++){
+    	cin >> A[i] >> B[i] >> C[i] ; 
     }
-    int ans = dfs(0 , 0);
-    cout << ans << endl;
-    return 0; 
+    cout << dfs(0,0) << endl;
+    return 0 ;
 }
