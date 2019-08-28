6e1c09d5108029c313557438c44c48667162f8db
Amjad Alhouseini <amjadm2008@gmail.com>
2019-08-28 13:20:00 +0300

    Add solution for problem `B - Uniqueness` Link: https://codeforces.com/contest/1208/problem/B Tags: binary search, brute force, implementation, two pointers, *1500 Ref: https://codeforces.com/contest/1208/submission/59609555

--- codeforces/1208/B.cpp
+++ codeforces/1208/B.cpp
@@ -70,15 +70,13 @@ int main(){
     int ans = n ;
     for(int i = 0; i < n; i++){
         set<int> s ;
-        bool ok = false ;
         for(int j = 0; j < i; j++){
             if ( s.find(v[j]) != s.end() ){
-                ok = true ;
+                i = n ;
                 break;
             }
             s.insert(v[j]) ;
         }
-        if ( ok ) break;
         for(int j = 0; j < n; j++){
             if ( s.find(v[n - j - 1]) != s.end() ){
                 ans = min(ans, n - (int) s.size() );
