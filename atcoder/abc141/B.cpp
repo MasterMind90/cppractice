7ef9e230d6d683ea10d948adf9bceb1d1d215ee2
Amjad Alhouseini <amjadm2008@gmail.com>
2019-09-19 06:19:00 +0300

    Add solution for problem `B - Tap Dance` Link: https://atcoder.jp/contests/abc141/tasks/abc141_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc141/submissions/7594033

---
+++ atcoder/abc141/B.cpp
@@ -0,0 +1,57 @@
+#ifndef LOCAL
+#pragma GCC optimize("O3")
+#endif
+#include "bits/stdc++.h"
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+#define fastio ios_base::sync_with_stdio(false);cin.tie(0);
+typedef long long ll;
+typedef tree<ll,null_type,less<ll>,rb_tree_tag,tree_order_statistics_node_update> indexed_set; 
+const ll INF = 1e9 + 10 ;
+const int N = 110 ; 
+int main(){
+    fastio
+    string s ; 
+    cin >> s ; 
+    int n = (int) s.size() ;
+    for(int i = 0; i < n; i += 2){
+        if ( s[i] == 'L' ) return cout << "No" << endl , 0;
+    }
+    for(int i = 1; i < n; i += 2){
+        if ( s[i] == 'R' ) return cout << "No" << endl , 0;
+    }
+    cout << "Yes" << endl;
+    return 0 ;
+}
+
+
+
