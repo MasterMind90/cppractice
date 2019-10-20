d0dee0012ee3e56df035dac57c225ae2eeca9b39
Amjad Alhouseini <amjadm2008@gmail.com>
2019-10-20 15:15:00 +0300

    Add solution for problem `D - Triangles` Link: https://atcoder.jp/contests/abc143/tasks/abc143_d Tags: AtCoder, *400 Ref: https://atcoder.jp/contests/abc143/submissions/8061920

---
+++ atcoder/abc143/D.cpp
@@ -0,0 +1,60 @@
+#ifndef LOCAL
+#pragma GCC optimize("O3")
+#endif
+#include "bits/stdc++.h"
+using namespace std;
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+#define fastio ios_base::sync_with_stdio(false) ; cin.tie(0) ;
+using ll = long long ;
+const ll INF = 1e10 + 10 ;
+
+int main(){
+    fastio
+    ll n ;
+    cin >> n ;
+
+    vector<ll> v(n) ;
+    for(ll i = 0; i < n; i++){
+        cin >> v[i] ;
+    }
+    sort(v.begin(), v.end()) ;
+    ll cnt = 0 ;
+    for(ll i = 0; i < n; i++){
+        for(ll j = i + 1; j < n; j++){
+            for(ll c = j + 1; c < n; c++){
+                if ( v[i] < v[j] + v[c] && v[j] < v[c] + v[i] && v[c] < v[i] + v[j] ) cnt++ ;
+            }
+        }
+    }
+    cout << cnt << endl;
+
+    return 0 ;
+}
+
