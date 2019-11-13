943b19aec8b96b9e65eeffa15ba626a107b837a7
Amjad Alhouseini <amjadm2008@gmail.com>
2019-11-13 21:51:00 +0200

    Add solution for problem `C - Dominated Subarray` Link: https://codeforces.com/contest/1257/problem/C Tags: greedy, implementation, sortings, strings, two pointers, *1200 Ref: https://codeforces.com/contest/1257/submission/64851503

---
+++ codeforces/1257/C.cpp
@@ -0,0 +1,70 @@
+#ifndef LOCAL
+#pragma GCC optimize("O3")
+#endif
+#include "bits/stdc++.h"
+using namespace std;
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+#define fastio ios_base::sync_with_stdio(false);cin.tie(0);
+typedef long long ll;
+const ll MOD = 1e9 + 7 ;
+const ll N = 1e5 + 10 ;
+int main(){
+    fastio
+    ll t ;
+    cin >> t ; 
+    while(t--){
+        ll n ; 
+        cin >> n ; 
+        vector<vector<ll> > v(n + 1) ;
+        vector<ll> a(n) ; 
+        vector<ll> cnt(n + 1) ; 
+        bool ok = false ;
+        for(ll i = 0; i < n; i++){
+            cin >> a[i] ; 
+            cnt[a[i]]++ ; 
+            if ( cnt[a[i]] > 1 ) ok = true ; 
+            v[a[i]].push_back(i) ; 
+        }
+        if ( !ok ) {
+            cout << -1 << endl;
+            continue ;
+        }
+        ll ans = n ; 
+        for(ll i = 0; i < n; i++){
+            vector<ll> &b = v[a[i]] ; 
+            ll m = (ll) b.size() ; 
+            ll index = upper_bound(b.begin(), b.end(), i) - b.begin() ; 
+            if ( index == m ) continue ;
+            ans = min(ans, b[index] - i + 1) ; 
+        }
+        cout << ans << endl;
+    }
+    return 0 ; 
+}
