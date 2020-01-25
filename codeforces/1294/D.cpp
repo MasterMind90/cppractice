6eccd12b00dd8be4d7b87d04884584d6594cce62
Amjad Alhouseini <amjadm2008@gmail.com>
2020-01-25 06:09:00 +0200

    Add solution for problem `D - MEX maximizing` Link: https://codeforces.com/contest/1294/problem/D Tags: data structures, greedy, implementation, math, *1600 Ref: https://codeforces.com/contest/1294/submission/69496241

---
+++ codeforces/1294/D.cpp
@@ -0,0 +1,59 @@
+#ifndef LOCAL
+#pragma GCC optimize("O3")
+#endif
+#include "bits/stdc++.h"
+using namespace std;
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << '\n'; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+#define fastio ios_base::sync_with_stdio(false);cin.tie(0);
+typedef long long ll;
+const ll MOD = 1e9 + 7 ;
+const ll N = 1e7 + 10 ;
+const ll INF = 1e18 + 10 ;
+int main(){
+    ll q , x ;
+    scanf("%I64d%I64d", &q, &x);
+    vector<ll> cnt(x) ;
+    set<pair<ll, ll> > s ; 
+    for(ll i = 0; i < x; i++){
+        s.insert(make_pair(0, i)) ;
+    }
+    while(q--){
+        ll a ; 
+        scanf("%I64d", &a);
+        ll k = a % x ; 
+        s.erase(make_pair(cnt[k], k)) ;
+        cnt[k]++ ;
+        s.insert(make_pair(cnt[k], k));
+        auto p = s.begin() ;
+        debug() << imie(*p) ;
+        cout << p->first * x + p->second << endl;
+    }
+    return 0; 
+}
