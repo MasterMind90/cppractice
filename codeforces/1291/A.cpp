eb6f596bc121b591d86cd92d0e849b8e5ee35341
Amjad Alhouseini <amjadm2008@gmail.com>
2020-02-02 16:22:00 +0200

    Add solution for problem `A - Even But Not Even` Link: https://codeforces.com/contest/1291/problem/A Tags: greedy, math, strings, *900 Ref: https://codeforces.com/contest/1291/submission/70042060

--- codeforces/1291/A.cpp
+++ codeforces/1291/A.cpp
@@ -1,33 +1,84 @@
-#include <bits/stdc++.h>
-
+#ifndef LOCAL
+#pragma GCC optimize("O3")
+#endif
+#include "bits/stdc++.h"
 using namespace std;
-
-int main(){
-    int t ;
-    cin >> t ;
-    while(t--){
-        int n ;
-        cin >> n ;
-        string s ;
-        cin >> s ;
-        int odd = 0 ;
-        for(int i = 0; i < n; i++){
-            if ( s[i] % 2 == 1 ) odd++;
-        }
-        if ( odd < 2 ){
-            cout << -1 << endl;
-        }
-        else{
-            int cnt = 0 ;
-            for(int i = 0; i < n; i++){
-                if ( cnt < 2 && s[i] % 2 == 1 ){
-                    cout << s[i] ;
-                    cnt++;
-                }
-            }
-            cout << endl;
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << '\n'; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+#define fastio ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+typedef long long ll;
+const ll MOD = 1e9 + 7 ;
+const ll N = 1e7 + 10 ;
+const ll INF = 1e9 + 10 ;
+void solve(){
+    int n ; 
+    cin >> n ; 
+    string s ; 
+    cin >> s ; 
+    while(not s.empty() && s.back() % 2 == 0) s.pop_back() ; 
+    if ( s.empty() ) {
+        cout << -1 << endl;
+        return ; 
+    }
+    int sum = 0 ; 
+    for(char c : s ) sum += c - '0' ; 
+    if ( sum % 2 == 0 ){
+        cout << s << endl;
+        return ; 
+    }
+    n = (int)s.size() ; 
+    debug() << imie(n) imie(s) ; 
+    int index = -1 ; 
+    for(int i = n - 2; i >= 0; i--){
+        if ( (s[i] - '0') % 2 == 1 ){
+            index = i ; 
+            break ; 
         }
     }
-
-    return 0 ;
+    if ( index == -1 ){
+        cout << index << endl;
+        return ; 
+    }
+    if ( s[0] == '0' ) {
+        cout << -1 << endl; 
+        return ; 
+    }
+    for(int i = 0; i < n; i++){
+        if ( i == index ) continue ; 
+        cout << s[i] ; 
+    }
+    cout << endl;
+}
+int main(){
+    fastio 
+    int t ;
+    cin >> t ;
+    while(t--) solve() ; 
+    return 0 ; 
 }
