1b1e677ff577b08b6389bf6bd7493331758e59b6
Amjad Alhouseini <amjadm2008@gmail.com>
2020-02-08 17:16:00 +0200

    Add solution for problem `C - Two Arrays` Link: https://codeforces.com/contest/1288/problem/C Tags: combinatorics, dp, *1600 Ref: https://codeforces.com/contest/1288/submission/70574491

---
+++ codeforces/1288/C.cpp
@@ -0,0 +1,67 @@
+#ifndef LOCAL
+#pragma GCC optimize("O3")
+#endif
+#include "bits/stdc++.h"
+using namespace std;
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << '\n'; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+#define fastio ios_base::sync_with_stdio(false);cin.tie(0);
+typedef long long ll;
+const ll MOD = 1e9 + 7 ;
+const ll N = 1e7 + 10 ;
+const ll INF = 1e18 + 10 ;
+#define int long long
+int n , m ; 
+int dp[25][1005] ;
+int dfs(int x, int last){
+    if ( x == 2 * m ) {
+        return 1 ;
+    }
+    if ( dp[x][last] != -1 ) return dp[x][last] ;
+    int ans = 0 ; 
+    if ( x == 0 && last == 0 ){
+        for(int i = 1; i <= n; i++){
+            ans = (ans % MOD + dfs(x + 1, i) % MOD) % MOD ; 
+        }
+    }
+    else{
+        for(int i = 0; i <= n && last + i <= n; i++){
+            ans = (ans % MOD + dfs(x + 1, last + i) % MOD) % MOD ; 
+        }
+    }
+    return dp[x][last] = ans % MOD ;
+}
+signed main(){
+    fastio
+    memset(dp, -1, sizeof dp) ;
+    cin >> n >> m ; 
+    int ans = dfs(0, 0) ;
+    cout << ans << endl;
+    return 0; 
+}
