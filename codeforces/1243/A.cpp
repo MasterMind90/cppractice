b27fa5ef6ec0915c766407574a10554e05097444
Amjad Alhouseini <amjadm2008@gmail.com>
2020-02-11 11:46:00 +0200

    Add solution for problem `A - Maximum Square` Link: https://codeforces.com/contest/1243/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/1243/submission/70778507

--- codeforces/1243/A.cpp
+++ codeforces/1243/A.cpp
@@ -9,14 +9,12 @@ int main(){
         int n ;
         cin >> n ;
         vector<int> v(n) ;
-        int nax = 0 ;
         for(int i = 0; i < n; i++){
             cin >> v[i] ;
-            nax = max(nax, v[i]) ;
         }
         sort(v.begin(), v.end()) ;
         int ans = 0 ;
-        for(int square = 1; square <= nax; square++){
+        for(int square = 1; square <= 1000; square++){
             int index = lower_bound(v.begin(), v.end(), square) - v.begin() ;
             int cnt = n - index ;
             if ( cnt >= square ){
