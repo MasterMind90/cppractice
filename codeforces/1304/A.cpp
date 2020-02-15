4d28ca32542541186c31424e372ddb66b9f9d3b3
Amjad Alhouseini <amjadm2008@gmail.com>
2020-02-15 15:12:00 +0200

    Add solution for problem `A - Two Rabbits` Link: https://codeforces.com/contest/1304/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1304/submission/71129070

--- codeforces/1304/A.cpp
+++ codeforces/1304/A.cpp
@@ -1,33 +1,57 @@
-#include <bits/stdc++.h>
-
+#ifndef LOCAL
+#pragma GCC optimize("O3")
+#endif
+#include "bits/stdc++.h"
 using namespace std;
-
+#define sim template < class c
+#define ris return * this
 #define int long long
-
-signed main(){
-    int t ;
-    cin >> t ;
-    while(t--){
-        int x , y , a , b ;
-        cin >> x >> y >> a >> b ;
-        int L = 0, R = 1e9 ;
-        int ans = -1 ;
-        while(L <= R){
-            int sec = ( L + R ) / 2 ;
-            int posA = x + sec * a ;
-            int posB = y - sec * b ;
-            if ( posA == posB ){
-                ans = sec ;
-                break;
-            }
-            if ( posA > posB ){
-                R = sec - 1 ;
-            }
-            else L = sec + 1 ;
-        }
-        cout << ans << endl;
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << '\n'; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+#define fastio ios_base::sync_with_stdio(false);cin.tie(0);
+typedef long long ll;
+const ll MOD = 1e9 + 7 ;
+const ll N = 1e5 + 10 ;
+const ll INF = 1e18 + 10 ;
+void solve(){
+    int x , y , a , b ; 
+    cin >> x >> y >> a >> b ; 
+    int dist = y - x ; 
+    if ( dist % (a + b) != 0 ){
+        cout << -1 << endl;
     }
-
+    else {
+        cout << dist / (a + b) << endl;
+    }
+}
+signed main(){
+    fastio
+    int t ;
+    cin >> t ; 
+    while(t--) solve() ; 
     return 0 ;
 }
-
