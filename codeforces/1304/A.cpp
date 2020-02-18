c035a113957d49ee2bd1bff8921d9d98414f4252
Amjad Alhouseini <amjadm2008@gmail.com>
2020-02-18 11:50:00 +0200

    Add solution for problem `A - Two Rabbits` Link: https://codeforces.com/contest/1304/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1304/submission/71367687

--- codeforces/1304/A.cpp
+++ codeforces/1304/A.cpp
@@ -1,29 +1,33 @@
 #include <bits/stdc++.h>
+
 using namespace std;
+
 #define int long long
+
 signed main(){
     int t ;
     cin >> t ;
     while(t--){
         int x , y , a , b ;
         cin >> x >> y >> a >> b ;
-        int L = 0, R = 2e9 ;
-        int ans = -1;
+        int L = 0, R = 1e9 ;
+        int ans = -1 ;
         while(L <= R){
-            int mid = L + (R - L) / 2 ;
-            int posX = x + mid * a ;
-            int posY = y - mid * b ;
-            if ( posX == posY ){
-                ans = mid ;
+            int sec = ( L + R ) / 2 ;
+            int posA = x + sec * a ;
+            int posB = y - sec * b ;
+            if ( posA == posB ){
+                ans = sec ;
                 break;
             }
-            if ( posX > posY ){
-                R = mid - 1 ;
+            if ( posA > posB ){
+                R = sec - 1 ;
             }
-            else L = mid + 1 ;
+            else L = sec + 1 ;
         }
         cout << ans << endl;
     }
 
     return 0 ;
 }
+
