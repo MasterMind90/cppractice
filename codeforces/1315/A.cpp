486b673fec5d3ddeb111b239bc65bb7197b690d8
Amjad Alhouseini <amjadm2008@gmail.com>
2020-02-23 18:15:00 +0200

    Add solution for problem `A - Dead Pixel` Link: https://codeforces.com/contest/1315/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/1315/submission/71704372

--- codeforces/1315/A.cpp
+++ codeforces/1315/A.cpp
@@ -1,21 +1,51 @@
-#include <bits/stdc++.h>
-
+#ifndef LOCAL
+#pragma GCC optimize("O3")
+#endif
+#include "bits/stdc++.h"
 using namespace std;
-
+#define sim template < class c
+#define ris return * this
 #define int long long
-
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << '\n'; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+#define fastio ios_base::sync_with_stdio(false);cin.tie(0);
+typedef long long ll;
+const ll MOD = 1e9 + 7 ;
+const ll N = 1e7 + 10 ;
+const ll INF = 1e18 + 10 ;
+void solve(){
+    int a , b , x , y ; 
+    cin >> a >> b >> x >> y ; 
+    cout << max({(x) * b , (y) * a , (a - x - 1) * b , (b - y - 1) * a }) << endl;
+}
 signed main(){
-    int t ;
+    fastio
+    int t ; 
     cin >> t ;
-    while(t--){
-        int a , b , x , y ;
-        cin >> a >> b >> x >> y ;
-        int up = x * b ;
-        int down = (a - x - 1) * b ;
-        int right = (b - y - 1) * a ;
-        int left = y * a ;
-        cout << max({up, down, right, left}) << endl;
-    }
-
-    return 0 ;
+    while(t--) solve() ;
+    return 0; 
 }
