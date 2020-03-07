542b00c0bcd0f38de281a636412ae6e1e463b0e7
Amjad Alhouseini <amjadm2008@gmail.com>
2020-03-07 19:57:00 +0200

    Add solution for problem `C - Tax Increase` Link: https://atcoder.jp/contests/abc158/tasks/abc158_c Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/abc158/submissions/10649064

--- atcoder/abc158/C.cpp
+++ atcoder/abc158/C.cpp
@@ -1,17 +1,11 @@
 #ifndef LOCAL
-#pragma GCC optimize("Ofast")
-#pragma GCC target("avx,avx2,fma")
-#pragma GCC optimization ("unroll-loops")
+#pragma GCC optimize("O3")
 #endif
 #include "bits/stdc++.h"
-#include <ext/pb_ds/assoc_container.hpp> // Common file
-#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
-#include <ext/pb_ds/detail/standard_policies.hpp>
 using namespace std;
-using namespace __gnu_pbds;
 #define sim template < class c
-#define int long long
 #define ris return * this
+#define int long long
 #define dor > debug & operator <<
 #define eni(x) sim > typename \
   enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
@@ -39,20 +33,20 @@ sim dor(const c&) { ris; }
 };
 #define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
 #define fastio ios_base::sync_with_stdio(false);cin.tie(0);
-typedef tree< pair<int,int>, null_type, less<pair<int,int> >, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
 typedef long long ll;
 const ll MOD = 1e9 + 7 ;
 const ll N = 2e5 + 10 ;
 const ll INF = 1e18 + 10 ;
+const double EPS = 1e-6 ; 
 signed main(){
     fastio
-    int a , b ;
-    cin >> a >> b ;
-    for(int price = 1; price < 2e6; price++){
-    	if ( (price * 8) / 100 == a && (price * 10) / 100 == b ){
-    		cout << price << endl;
-    		return 0 ;
-    	}
+    int a , b ; 
+    cin >> a >> b ; 
+    for(int i = 1; i < 1e6; i++){
+        if ( (i * 8) / 100 == a && (i * 10) / 100 == b  ){
+            cout << i << endl;
+            return 0 ; 
+        }
     }
     cout << -1 << endl;
     return 0; 
