bfbff4d76997dc2c1dd67b55ca8445f32ec4824a
Amjad Alhouseini <amjadm2008@gmail.com>
2020-03-17 17:25:00 +0200

    Add solution for problem `A - Reorder the Array` Link: https://codeforces.com/contest/1007/problem/A Tags: combinatorics, data structures, math, sortings, two pointers, *1300 Ref: https://codeforces.com/contest/1007/submission/73499672

---
+++ codeforces/1007/A.cpp
@@ -0,0 +1,58 @@
+#ifndef LOCAL
+#pragma GCC optimize("O3")
+#endif
+#include "bits/stdc++.h"
+using namespace std;
+#define sim template < class c
+#define ris return * this
+#define int long long
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << '\n'; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+#define fastio ios_base::sync_with_stdio(false);cin.tie(0);
+typedef long long ll;
+const ll MOD = 1e9 + 7 ;
+const ll N = 2e5 + 10 ;
+const ll INF = 1e18 + 10 ;
+signed main(){
+    fastio
+    int n ;
+    cin >> n ; 
+    vector<int> v(n) ; 
+    for(int i = 0; i < n; i++){
+        cin >> v[i] ; 
+    }
+    multiset<int> ms(v.begin(), v.end()) ;
+    int ans = 0 ;
+    for(int i = 0; i < n; i++){
+        auto it = ms.upper_bound(v[i]) ;
+        if ( it == ms.end() ) continue ; 
+        ans++ ; 
+        ms.erase(it) ;
+    }
+    cout << ans << endl;
+    return 0; 
+}
