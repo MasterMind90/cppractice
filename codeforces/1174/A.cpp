f0fa123eacfa1d1978fddf58823071061d674830
Amjad Alhouseini <amjadm2008@gmail.com>
2020-03-25 18:49:00 +0200

    Add solution for problem `A - Ehab Fails to Be Thanos` Link: https://codeforces.com/contest/1174/problem/A Tags: constructive algorithms, greedy, sortings, *1000 Ref: https://codeforces.com/contest/1174/submission/74315851

---
+++ codeforces/1174/A.cpp
@@ -0,0 +1,71 @@
+#ifndef LOCAL
+#pragma GCC optimize("O3")
+#endif
+#include "bits/stdc++.h"
+using namespace std;
+#define sim template < class c
+#define ris return * this
+#define int long long
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << '\n'; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+#define fastio ios_base::sync_with_stdio(false);cin.tie(0);
+typedef long long ll;
+const ll MOD = 1e9 + 7 ;
+const ll N = 1e6 + 10 ;
+const ll INF = 1e18 + 10 ;
+signed main(){
+    int n ;
+    cin >> n ; 
+    vector<int> v(2 * n) ; 
+    set<int> s ;
+    for(int i = 0; i < 2 * n; i++){
+        cin >> v[i] ; 
+        s.insert(v[i]) ;
+    }
+    if ( s.size() == 1 ){
+        cout << -1 << endl;
+        return 0;
+    }
+    sort(v.begin(), v.end()) ;
+    int sum = 0 ; 
+    for(int i = 0; i < n; i++){
+        sum += v[i] ; 
+    }
+    int ans = 0 ; 
+    for(int i = n; i < 2 * n; i++){
+        ans += v[i] ; 
+    }
+    if ( sum != ans ){
+        for(int i = 0; i < 2 * n; i++){
+            cout << v[i] << ' ' ; 
+        }
+        cout << endl;
+        return 0 ;
+    }
+    cout << -1 << endl ; 
+    return 0 ;
+}
