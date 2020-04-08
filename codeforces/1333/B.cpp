484adc14b046980964b66dbdcaed747e02a00094
Amjad Alhouseini <amjadm2008@gmail.com>
2020-04-08 23:41:00 +0300

    Add solution for problem `B - Kind Anton` Link: https://codeforces.com/contest/1333/problem/B Tags: greedy, implementation, *1100 Ref: https://codeforces.com/contest/1333/submission/75929930

---
+++ codeforces/1333/B.cpp
@@ -0,0 +1,64 @@
+#ifndef LOCAL
+#pragma GCC optimize("O3")
+#endif
+#include "bits/stdc++.h"
+using namespace std;
+#define sim template < class c
+#define ris return * this
+#define int long long
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << '\n'; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+#define fastio ios_base::sync_with_stdio(false);cin.tie(0);
+typedef long long ll;
+const ll MOD = 1e9 + 7 ;
+const ll N = 1e5 + 10 ;
+const ll INF = 1e18 + 10 ;
+void solve(){
+    int n ;
+    cin >> n ;
+    vector<int> a(n) , b(n) ; 
+    for(int i = 0; i < n; i++) cin >> a[i] ; 
+    for(int i = 0; i < n; i++) cin >> b[i] ; 
+    bool one = false;
+    bool minus = false;
+    bool ok = true;
+    for(int i = 0; i < n; i++){
+        if ( a[i] < b[i] && not one ) ok = false;
+        if ( a[i] > b[i] && not minus ) ok = false;
+        if ( a[i] == 1 ) one = true ;
+        if ( a[i] == -1 ) minus = true ;
+    }
+    if ( ok ) cout << "YES" << endl;
+    else cout << "NO" << endl;
+}
+signed main(){
+    fastio 
+    int t; 
+    cin >> t ;
+    while(t--) solve() ; 
+    return 0 ; 
+}
