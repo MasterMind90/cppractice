8dd9d77c3e7021c87943db688d8cb6e2a6f397ec
Amjad Alhouseini <amjadm2008@gmail.com>
2020-06-23 17:18:00 +0300

    Add solution for problem `B - AccurateLee` Link: https://codeforces.com/contest/1369/problem/B Tags: greedy, implementation, strings, *1200 Ref: https://codeforces.com/contest/1369/submission/84766230

---
+++ codeforces/1369/B.cpp
@@ -0,0 +1,77 @@
+#ifndef LOCAL
+#pragma GCC optimize("O3")
+#endif
+#include "bits/stdc++.h"
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+#include <ext/pb_ds/detail/standard_policies.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define sim template < class c
+#define int long long
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << '\n'; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+#define fastio ios_base::sync_with_stdio(false);cin.tie(0);
+typedef tree< pair<int,int>, null_type, less<pair<int,int> >, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+typedef long long ll;
+const ll MOD = 1e9 + 7 ;
+const ll N = 2e5 + 10 ;
+const ll INF = 1e18 + 10 ;
+void solve(){
+    int n ;
+    cin >> n ;
+    string s ;
+    cin >> s; 
+    int zero = -1 ;
+    int one = -1 ;
+    for(int i = 0; i < n; i++){
+        if ( s[i] == '1' ) {
+            one = i ;
+            break ;
+        }
+    }
+    for(int i = n - 1; i >= 0; i--){
+        if ( s[i] == '0' ) {
+            zero = i ; 
+            break ;
+        }
+    }
+    if ( zero != -1 && one != -1 && zero > one ){
+        s.erase(one, zero - one) ;
+    }
+    cout << s << endl;
+}
+signed main(){
+    fastio
+    int t ;
+    cin >> t ;
+    while(t--){
+        solve() ;
+    }
+    return 0; 
+}
