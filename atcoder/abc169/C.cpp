b983249fe173042df2e6fd52768bd9bc81cdd5fb
Amjad Alhouseini <amjadm2008@gmail.com>
2020-07-03 00:15:00 +0300

    Add solution for problem `C - Multiplication 3` Link: https://atcoder.jp/contests/abc169/tasks/abc169_c Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/abc169/submissions/14911154

--- atcoder/abc169/C.cpp
+++ atcoder/abc169/C.cpp
@@ -1,14 +1,66 @@
-#include <bits/stdc++.h>
-
+#ifndef LOCAL
+#pragma GCC optimize("O3")
+#endif
+#include "bits/stdc++.h"
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+#include <ext/pb_ds/detail/standard_policies.hpp>
 using namespace std;
+using namespace __gnu_pbds;
+#define sim template < class c
 #define int long long
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << '\n'; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+#define fastio ios_base::sync_with_stdio(false);cin.tie(0);
+typedef tree< pair<int,int>, null_type, less<pair<int,int> >, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+typedef long long ll;
+const ll MOD = 1e9 + 7 ;
+const ll N = 2e5 + 10 ;
+const ll INF = 1e18 + 10 ;
+int extract(string s){
+    string ans = "" ;
+    for(int i = 0; i < s.size(); i++){
+        if ( s[i] == '.' ) continue ;
+        ans.push_back(s[i]) ;
+    }
+    stringstream ss ;
+    ss << ans ; 
+    int x ;
+    ss >> x ; 
+    return x ;
+}
 signed main(){
-	int a ; 
-	cin >> a ;
-	string b ; 
-	cin >> b ; 
-	string t = b.erase(1, 1) ;
-	int s = stoi(t) ; 
-	cout << (a * s) / 100 << endl;
-	return 0 ;
+    fastio
+    int a ; 
+    string b ; 
+    cin >> a >> b ; 
+    int c = extract(b) ; 
+    int ans = a * c / 100 ; 
+    cout << ans << endl; 
+    return 0; 
 }
