c66deda5add6597f3f853213d594b9cbfce416af
Amjad Alhouseini <amjadm2008@gmail.com>
2020-07-10 15:04:00 +0300

    Add solution for problem `B - Berland Bingo` Link: https://codeforces.com/contest/370/problem/B Tags: implementation, *1300 Ref: https://codeforces.com/contest/370/submission/86424680

---
+++ codeforces/370/B.cpp
@@ -0,0 +1,77 @@
+#ifndef LOCAL
+#pragma GCC optimize("O3")
+#endif
+#include "bits/stdc++.h"
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+#include <ext/pb_ds/detail/standard_policies.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define sim template < class c
+#define int long long
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << '\n'; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+#define fastio ios_base::sync_with_stdio(false);cin.tie(0);
+typedef tree< pair<int,int>, null_type, less<pair<int,int> >, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+typedef long long ll;
+const ll MOD = 1e9 + 7 ;
+const ll N = 2e5 + 10 ;
+const ll INF = 1e18 + 10 ;
+signed main(){
+    fastio
+    int n ; 
+    cin >> n ;
+    set<int> s[n] ;
+    for(int i = 0; i < n; i++){
+        int m ;
+        cin >> m ;
+        for(int j = 0; j < m; j++){
+            int x ;
+            cin >> x ;
+            s[i].insert(x) ;
+        }
+    }
+    for(int i = 0; i < n; i++){
+        bool ok = true ;
+        for(int j = 0; j < n; j++){
+            if ( i == j ) continue ;
+            if ( s[i].size() < s[j].size() ) continue ;
+            int cnt = 0 ; 
+            for(int c : s[j]){
+                if ( s[i].count(c)  ) cnt++ ;
+            }
+            debug() << imie(i) imie(j) imie(cnt) ;
+            if ( cnt == s[j].size() ) ok = false ;
+        }
+        if ( ok ) {
+            cout << "YES" << endl ;
+        }
+        else cout << "NO" << endl;
+    }
+    return 0; 
+}
