ff8c68e61f1446a85bd43203e121ac131a7a670e
Amjad Alhouseini <amjadm2008@gmail.com>
2020-08-19 19:24:00 +0300

    Add solution for problem `K - Stones` Link: https://atcoder.jp/contests/dp/tasks/dp_k Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/16047477

---
+++ atcoder/dp/K.cpp
@@ -0,0 +1,76 @@
+#ifndef LOCAL
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimization ("unroll-loops")
+#endif
+#include "bits/stdc++.h"
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+#include <ext/pb_ds/detail/standard_policies.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define sim template < class c
+#define int long long
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << '\n'; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+#define fastio ios_base::sync_with_stdio(false);cin.tie(0);
+typedef tree< pair<int,int>, null_type, less<pair<int,int> >, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+typedef long long ll;
+const ll MOD = 1e9 + 7 ;
+const ll N = 1e5 + 10 ;
+const ll INF = 1e18 + 10 ;
+int n , k ; 
+vector<int> v ;
+int dp[N] ;
+int dfs(int cur){
+    if ( cur == 0 ) {
+        return 0 ;
+    }
+    if ( dp[cur] != -1 ) return dp[cur] ;
+    bool ans = false ;
+    for(int i = 0; i < n; i++){
+        if ( v[i] <= cur ){
+            if ( not dfs(cur - v[i] ) ){
+                ans = true ;
+            }
+        }
+    }
+    return dp[cur] = ans ;
+}
+signed main(){
+    fastio
+    memset(dp, -1, sizeof dp) ;
+    cin >> n >> k ; 
+    v = vector<int>(n) ;
+    for(int i = 0; i < n; i++){
+        cin >> v[i] ;
+    }
+    int ans = dfs(k) ;
+    puts(ans ? "First" : "Second") ;
+    return 0; 
+}
