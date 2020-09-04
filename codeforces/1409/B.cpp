44f6764afb7c9cdef539839ab8bedf67f965c30c
Amjad Alhouseini <amjadm2008@gmail.com>
2020-09-04 17:45:00 +0300

    Add solution for problem `B - Minimum Product` Link: https://codeforces.com/contest/1409/problem/B Tags: brute force, greedy, math, *1100 Ref: https://codeforces.com/contest/1409/submission/91814580

---
+++ codeforces/1409/B.cpp
@@ -0,0 +1,85 @@
+#ifndef LOCAL
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimization ("unroll-loops")
+#endif
+#include "bits/stdc++.h"
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+#include <ext/pb_ds/detail/standard_policies.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define sim template < class c
+#define int long long
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << '\n'; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+#define fastio ios_base::sync_with_stdio(false);cin.tie(0);
+typedef tree< pair<int,int>, null_type, less<pair<int,int> >, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+typedef long long ll;
+const ll MOD = 1e9 + 7 ;
+const ll N = 2e5 + 10 ;
+const ll INF = 1e18 + 10 ;
+void solve(){
+    int a , b , x , y , n ; 
+    cin >> a >> b >> x >> y >> n ;
+    if ( a > b ) {
+        swap(a, b) ;
+        swap(x, y) ;
+    }
+    int xx = n ;
+    int aa = a ;
+    int bb = b ;
+    int ans = INF ;
+    {
+        int nix = min(n, a - x) ;
+        a -= nix ;
+        n -= nix ;
+        nix = min(n, b - y) ;
+        b -= nix ;
+        ans = a * b ;
+    }
+    {
+        int nix = min(xx, bb - y) ;
+        bb -= nix ;
+        xx -= nix ;
+        nix = min(xx, aa - x) ;
+        aa -= nix ;
+        ans = min(ans, aa * bb);
+    }
+    cout << ans << endl;
+
+}
+signed main(){
+    fastio
+    int t ;
+    cin >> t ;
+    while(t--) {
+        solve() ;
+    }
+    return 0; 
+}
