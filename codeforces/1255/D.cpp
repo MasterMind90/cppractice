2a753c7a787757e616f4bd8690d3a0cb577c4cf0
Amjad Alhouseini <amjadm2008@gmail.com>
2020-09-12 00:00:00 +0300

    Add solution for problem `D - Feeding Chicken` Link: https://codeforces.com/contest/1255/problem/D Tags: greedy, implementation, *1700 Ref: https://codeforces.com/contest/1255/submission/92531870

---
+++ codeforces/1255/D.cpp
@@ -0,0 +1,116 @@
+#ifndef LOCAL
+#pragma GCC optimize("O3")
+#endif
+#include "bits/stdc++.h"
+using namespace std;
+#define int long long
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << '\n'; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+#define fastio ios_base::sync_with_stdio(false);cin.tie(0)
+typedef long long ll;
+const ll MOD = 1e9 + 7 ;
+const ll N = 1e7 + 1 ;
+const ll INF = 1e18 + 10 ;
+void solve(){
+    int n , m , k ;
+    cin >> n >> m >> k ; 
+    vector<vector<char> > g(n, vector<char>(m)) ;
+    int R = 0 ;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            cin >> g[i][j] ;
+            if ( g[i][j] == 'R' ) R++ ;
+        }
+    }
+    int each = R / k ;
+    int extra = R % k ; 
+    debug() << imie(R) imie(k) ;
+    debug() << imie(each) imie(extra) ;
+    vector<vector<char> > ans(n, vector<char>(m)) ;
+    auto decode = [&](int x){
+        if ( x < 10 ) return char(x + '0') ;
+        if ( x < 10 + 26 ) return char(x - 10 + 'a') ; 
+        return char(x - 10 - 26 + 'A') ;
+    };
+    int cur = 0 ;
+    int cnt = 0 ; 
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            int x = j ;
+            if ( i % 2 == 1 ) x = m - j - 1 ;
+            ans[i][x] = decode(cur) ;
+            if ( g[i][x] == 'R' ) {
+                cnt++ ;
+            }
+            if ( extra ) {
+                if ( cnt == each + 1 ){
+                    cnt = 0 ;
+                    extra--;
+                    cur++ ;
+                    cur = min(cur, k - 1) ;
+                }
+            }
+            else{
+                if ( cnt == each ) {
+                    cnt = 0 ; 
+                    cur++ ;
+                    cur = min(cur, k - 1) ;
+                }
+            }
+        }
+    }
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            cout << ans[i][j] ;
+        }
+        cout << endl; 
+    }
+}
+signed main(){
+    fastio;
+    int t ;
+    cin >> t ;
+    while(t--) solve() ;
+    return 0; 
+}
+
+//3
+//
+//........1.......................
+//................................
+//......................3.........
+//..............2.................
+//................................
+//..1.............................
+//..............2.................
+//................................
+//................................
+//................................
+//................................
+//................................
