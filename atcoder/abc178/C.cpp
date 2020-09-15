d72a8881ef8df7987014f02299459088ff4448ff
Amjad Alhouseini <amjadm2008@gmail.com>
2020-09-15 11:48:00 +0300

    Add solution for problem `C - Ubiquity` Link: https://atcoder.jp/contests/abc178/tasks/abc178_c Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/abc178/submissions/16762771

--- atcoder/abc178/C.cpp
+++ atcoder/abc178/C.cpp
@@ -1,58 +1,79 @@
-#include <bits/stdc++.h>
+#ifndef LOCAL
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimization ("unroll-loops")
+#endif
+#include "bits/stdc++.h"
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+#include <ext/pb_ds/detail/standard_policies.hpp>
 using namespace std;
+using namespace __gnu_pbds;
+#define sim template < class c
 #define int long long
-const int MOD = 1e9 + 7 ;
-const int N = 1e6 + 10 ;
-vector<int> fact(N) ;
-int fast(int a, int b){
-	if ( b == 0 ) return 1 ;
-	if ( b == 1 ) return a ;
-	int y = fast(a, b / 2) ;
-	y *= y ;
-	y %= MOD ;
-	if ( b & 1 ) {
-		y *= a ;
-		y %= MOD ;
-	}
-	return y ;
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << '\n'; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
 }
-int inv(int x){
-	return fast(x, MOD - 2) ;
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
 }
-int nCk(int n, int k){
-	int ans = fact[n] ;
-	ans *= inv(fact[n - k]) ;
-	ans %= MOD ;
-	ans *= inv(fact[k]) ;
-	ans %= MOD ;
-	return ans ;
-}
-int fix(int x){
-	return ((x % MOD) + MOD) % MOD ;
-}
-int S(int n){
-	int ans = 0 ;
-	for(int x = 2; x <= n; x++){
-		int res = fix(fast(2, x) - 2) ;
-		res %= MOD ;
-		res *= fast(8, n - x) ;
-		res %= MOD ;
-		res *= nCk(n, x) ;
-		res %= MOD ;
-		ans += res ;
-		ans %= MOD ;
-	}
-	return ans ;
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+#define fastio ios_base::sync_with_stdio(false);cin.tie(0);
+typedef tree< pair<int,int>, null_type, less<pair<int,int> >, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+typedef long long ll;
+const ll MOD = 1e9 + 7 ;
+const ll N = 1e6 + 10 ;
+const ll INF = 1e18 + 10 ;
+int n ; 
+int dp[N][2][2] ; 
+int dfs(int x, int a, int b){
+    if ( x == n ) {
+        if ( a == 1 && b == 1 ) {
+            return 1 ;
+        }
+        return 0 ; 
+    }
+    if ( dp[x][a][b] != -1 ) return dp[x][a][b] ;
+    int ans = 0 ;
+    for(int i = 0; i < 10; i++){
+        if ( i == 0 ) {
+            ans += dfs(x + 1, 1, b) ;
+        }
+        else if ( i == 9 ) {
+            ans += dfs(x + 1, a, 1) ;
+        }
+        else{
+            ans += dfs(x + 1, a, b) ;
+        }
+        ans %= MOD ;
+    }
+    return dp[x][a][b] = ans ;
 }
 signed main(){
-	fact[0] = fact[1] = 1 ;
-	for(int i = 2; i < N; i++){
-		fact[i] = i * fact[i - 1] ;
-		fact[i] %= MOD ;
-	}
-	int n ;
-	cin >> n ;
-	int ans = S(n) ;
-	cout << ans << endl;
-	return 0 ;
+    fastio
+    memset(dp, -1, sizeof dp) ;
+    cin >> n ;
+    int ans = dfs(0, 0, 0) ;
+    cout << ans << endl;
+    return 0; 
 }
