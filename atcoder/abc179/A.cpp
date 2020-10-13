be38c8d7ac11184ea76f4cd69bfb0a356fc1b869
Amjad Alhouseini <amjadm2008@gmail.com>
2020-10-13 11:00:00 +0300

    Add solution for problem `A - Plural Form` Link: https://atcoder.jp/contests/abc179/tasks/abc179_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc179/submissions/17378315

---
+++ atcoder/abc179/A.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string s ;
+    cin >> s ;
+    if ( s[s.size() - 1] == 's' ) {
+        s += "es" ;
+    }
+    else s += "s" ;
+    cout << s << endl;
+
+    return 0 ;
+}
