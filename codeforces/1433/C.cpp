3ed20b3eefb5c4118f384ed3db590fdc7bc1becd
Amjad Alhouseini <amjadm2008@gmail.com>
2020-10-20 17:52:00 +0300

    Add solution for problem `C - Dominant Piranha` Link: https://codeforces.com/contest/1433/problem/C Tags: constructive algorithms, greedy, *900 Ref: https://codeforces.com/contest/1433/submission/96100485

---
+++ codeforces/1433/C.cpp
@@ -0,0 +1,99 @@
+#ifndef LOCAL
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimization ("unroll-loops")
+#endif
+#include "bits/stdc++.h"
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+#include <ext/pb_ds/detail/standard_policies.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define sim template < class c
+#define int long long
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << '\n'; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+#define fastio ios_base::sync_with_stdio(false);cin.tie(0);
+typedef tree< pair<int,int>, null_type, less<pair<int,int> >, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+typedef long long ll;
+const ll MOD = 1e9 + 7 ;
+const ll N = 2e5 + 10 ;
+const ll INF = 1e18 + 10 ;
+void solve(){
+    int n ;
+    cin >> n ;
+    vector<int> v(n) ;
+    int nax = 0 ;
+    for(int i = 0; i < n; i++){
+        cin >> v[i] ;
+        if ( v[i] > nax ) {
+            nax = v[i] ;
+        }
+    }
+    bool same = true ;
+    for(int i = 1; i < n; i++){
+        if ( v[i] != v[i - 1] ) same = false ;
+    }
+    if ( same ) {
+        cout << -1 << endl;
+        return ;
+    }
+    for(int i = 0; i < n; i++){
+        if ( v[i] == nax ){
+            if ( i == 0 ) {
+                if ( v[i + 1] != nax ) {
+                    cout << i + 1 << endl;
+                    return ;
+                }
+            }
+            else if ( i == n - 1 ){
+                if ( v[i - 1] != nax ) {
+                    cout << i + 1 << endl;
+                    return ;
+                }
+            }
+            else{
+                if ( v[i - 1] != nax ) {
+                    cout << i + 1 << endl;
+                    return ;
+                }
+                if ( v[i + 1] != nax ){
+                    cout << i + 1 << endl;
+                    return ;
+                }
+            }
+        }
+    }
+}
+signed main(){
+    fastio
+    int t ;
+    cin >> t ;
+    while(t--) solve() ;
+    return 0; 
+}
