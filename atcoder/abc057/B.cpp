dec4a918e80a41d40a1311da82121363780b92ef
Amjad Alhouseini <amjadm2008@gmail.com>
2020-11-29 10:35:00 +0200

    Add solution for problem `B - Checkpoints` Link: https://atcoder.jp/contests/abc057/tasks/abc057_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc057/submissions/18486419

--- atcoder/abc057/B.cpp
+++ atcoder/abc057/B.cpp
@@ -1,54 +1,76 @@
-#include <iostream>
-#include <climits>
-#include <vector>
-#include <cmath>
+#ifndef LOCAL
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimization ("unroll-loops")
+#endif
+#include "bits/stdc++.h"
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+#include <ext/pb_ds/detail/standard_policies.hpp>
 using namespace std;
- 
- 
-int main(){
-    
-    int N,M;
-    cin >> N >> M;
- 
-    vector <pair<long long ,long long > > S(N);
-    vector <pair <long long, pair<long long ,long long>>> P(M);
- 
-    for (long long  i = 0; i < N; i++)
-        cin >> S[i].first >> S[i].second;
- 
-    for (long long j = 0; j < M; j++){
-        P[j].first = j+1;
-        cin >> P[j].second.first >> P[j].second.second;
-    }
- 
-    for (long long i = 0; i < N; i++){
-        long long Mndistance = LLONG_MAX;
-        long long  Mnindex = M;
- 
-        for (long long j = 0; j < M; j++){
- 
-            long long x1 = S[i].first;
-            long long y1 = S[i].second;
-            long long x2 = P[j].second.first;
-            long long y2 = P[j].second.second;
-            long long distance = abs(x1 - x2) + abs(y1 - y2);
- 
-            if (Mndistance > distance){
-                //cout  << " distances:"<< Mndistance << "   " << distance << endl;
-                Mndistance = distance;
-                //cout << "indexes" << index << " " << P[j].first << endl;
-                Mnindex = P[j].first;
- 
-            }
- 
-            //if (Mndistance == distance) Mnindex = min(index,P[j].first);
-        }
-        cout << Mnindex << endl;
-    }
- 
-    
- 
-    
- 
-    return 0;
+using namespace __gnu_pbds;
+#define sim template < class c
+#define int long long
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << '\n'; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
 }
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+#define fastio ios_base::sync_with_stdio(false);cin.tie(0);
+typedef tree< pair<int,int>, null_type, less<pair<int,int> >, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+typedef long long ll;
+const ll MOD = 1e9 + 7 ;
+const ll N = 2e5 + 10 ;
+const ll INF = 1e18 + 10 ;
+signed main(){
+    fastio
+    int n , m ; 
+    cin >> n >> m ; 
+    vector<pair<int,int> > v(n) ;
+    for(int i = 0; i < n; i++){
+    	cin >> v[i].first >> v[i].second ; 
+    }
+    vector<pair<int,int> > p(m) ;
+    for(int i = 0; i < m; i++){
+    	cin >> p[i].first >> p[i].second;
+    }
+    for(int student = 0; student < n; student++){
+    	int nix = INF ;
+    	int checkpoint = -1 ;
+    	for(int mall = 0; mall < m; mall++){
+    		int dist = abs(v[student].first - p[mall].first) + abs(v[student].second - p[mall].second) ;
+    		if ( dist < nix ) {
+    			nix = dist ; 
+    			checkpoint = mall + 1; 
+    		}
+    	}
+    	cout << checkpoint << endl;
+    }
+    return 0; 
+}
+
+// (a, b) , (a1, b2) , ..... 
+
+// (x1, y1) , (x2, y2) , ..... 
