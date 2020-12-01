9549a646cc22bf890ed488a02bbbec7aafaad5c9
Amjad Alhouseini <amjadm2008@gmail.com>
2020-12-01 11:44:00 +0200

    Add solution for problem `C - Multiplication 3` Link: https://atcoder.jp/contests/abc169/tasks/abc169_c Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/abc169/submissions/18514690

--- atcoder/abc169/C.cpp
+++ atcoder/abc169/C.cpp
@@ -2,21 +2,13 @@
 
 using namespace std;
 #define int long long
-int toInt(string s = "123"){
-	int ans = 0 ; 
-	for(int i = 0; i < s.size(); i++){
-		ans *= 10 ;
-		ans += s[i] - '0' ; 
-	}
-	return ans ;
-}
 signed main(){
 	int a ; 
 	cin >> a ;
 	string b ; 
 	cin >> b ; 
 	string t = b.erase(1, 1) ;
-	int s = toInt(t) ; 
+	int s = stoi(t) ; 
 	cout << (a * s) / 100 << endl;
 	return 0 ;
 }
