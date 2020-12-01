cd6def4077e9ed3fd224b158b3bfac1b4d8dd494
Amjad Alhouseini <amjadm2008@gmail.com>
2020-12-01 12:41:00 +0200

    Add solution for problem `B - Guidebook` Link: https://atcoder.jp/contests/abc128/tasks/abc128_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc128/submissions/18515423

--- atcoder/abc128/B.cpp
+++ atcoder/abc128/B.cpp
@@ -3,6 +3,19 @@
 using namespace std;
 #define int long long
 
+bool comp(pair <string,pair<int,int>> a, pair<string,pair<int,int>> b){
+	if (a.first < b.first)
+		return true;
+	else if (a.first > b.first)
+		return false;
+	else {
+		if (a.second.first > b.second.first)
+			return true;
+
+		else return false;
+	}
+}
+
 signed main(){
 	
 	int n;
@@ -11,11 +24,10 @@ signed main(){
 	vector <pair <string,pair< int , int >>> city(n);
 	for (int i = 0; i < n; i++){
 		cin >> city[i].first >> city[i].second.first;
-		city[i].second.first = -city[i].second.first ;
 		city[i].second.second = i+1;
 	}
 
-	sort (city.begin(),city.end());
+	sort (city.begin(),city.end(),comp);
 
 	for (int i = 0; i < n; i++){
 		cout << city[i].second.second << endl;
