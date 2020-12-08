00997994de27d208849044b919615a753db48c8c
Amjad Alhouseini <amjadm2008@gmail.com>
2020-12-08 12:08:00 +0200

    Add solution for problem `C - Walking Takahashi` Link: https://atcoder.jp/contests/abc175/tasks/abc175_c Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/abc175/submissions/18642140

--- atcoder/abc175/C.cpp
+++ atcoder/abc175/C.cpp
@@ -1,70 +1,68 @@
-#include <bits/stdc++.h>
+#ifndef LOCAL
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimization ("unroll-loops")
+#endif
+#include "bits/stdc++.h"
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+#include <ext/pb_ds/detail/standard_policies.hpp>
 using namespace std;
- 
-long long max(long long a , long long b){
- 
-	if (a >= b)
-		return a;
- 
-	else return b;
- 
+using namespace __gnu_pbds;
+#define sim template < class c
+#define int long long
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << '\n'; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
 }
- 
-long long min(long long a , long long b){
- 
-	if (a <= b)
-		return a;
- 
-	else return b;
- 
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
 }
- 
-long long absv(long long x){
-	if (x >= 0)
-		return x;
- 
-	else return -1*x;
-}
- 
-/////////////////////// x = 1 , k = 5 , d = 3 //////////////////////////
-////////// -10 -9 -8 -7 -6 -5 -4 -3 -2 -1 0  1  2  3  4  5  6  7  8  9  10 ///////////
- 
-int main(){
-	long long x , k , d;
-	cin >> x >> k >> d;
-	x = abs(x) ;
-	long long ok = k;
- 
-	if (x > 0){
- 
-		k -= min(k,absv(x/d));
-		x -= d*min(ok,(x/d));
- 
-		if (k%2 == 0)
-			cout << x << endl;
- 
-		else cout <<  min(absv(x - d),absv(x+d)) << endl;
- 
- 
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+#define fastio ios_base::sync_with_stdio(false);cin.tie(0);
+typedef tree< pair<int,int>, null_type, less<pair<int,int> >, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+typedef long long ll;
+const ll MOD = 1e9 + 7 ;
+const ll N = 2e5 + 10 ;
+const ll INF = 1e18 + 10 ;
+signed main(){
+    fastio
+    int x , k , d ;
+    cin >> x >> k >> d ;
+    x = abs(x) ;
+    if ( x == 0 ) {
+    	if ( k & 1 ) {
+    		cout << d << endl;
+    	}
+    	else cout << 0 << endl;
+    	return 0; 
+    }
+    // 7 4 3
+    int c = min(k, x / d) ;
+    k -= c ;
+	x -= c * d ;
+	if ( k & 1 ) {
+		cout << abs(x - d) << endl;
 	}
- 
-	else if (x < 0){
- 
-		k -= min(k,x/d);
-		x -= d*min(ok,absv(x/d));
- 
-		if (k%2 == 0) cout << absv(x) << endl;
-		else cout << min(absv(x - d),absv(x+d)) ;
- 
- 
-	}
- 
-	else {
- 
-		if (k%2 == 0) cout << 0 << endl;
-		else cout << d << endl;
- 
-	}
- 
-	return 0;
+	else cout << x << endl;
+    return 0; 
 }
