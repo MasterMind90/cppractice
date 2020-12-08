344a68c65a53378365814d927a9e11bb9a31cd9c
Amjad Alhouseini <amjadm2008@gmail.com>
2020-12-08 11:27:00 +0200

    Add solution for problem `C - Cream puff` Link: https://atcoder.jp/contests/abc180/tasks/abc180_c Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/abc180/submissions/18641591

--- atcoder/abc180/C.cpp
+++ atcoder/abc180/C.cpp
@@ -3,26 +3,31 @@
 #include <algorithm>
 using namespace std;
 typedef long long ll;
- 
+
 int main(){
- 
+
 	ll n;
 	cin >> n;
- 
+
 	vector <ll> v;
- 
+
 	for (ll i = 1; i * i <= n;i++)
 		if(n%i == 0){
-			cout << i << endl;
-			if (n/i != i)
-			v.push_back(n/i);		
+			if (n/i == i) {
+				v.push_back(i);
+			}
+			else {
+                v.push_back(i);
+                v.push_back(n/i);
+			}
+
 	}
- 
-	reverse (v.begin(),v.end());
- 
+
+	sort (v.begin(),v.end());
+
 	for (auto x : v)
 		cout << x << endl;
- 
+
 	return 0;
-		
+
 }
