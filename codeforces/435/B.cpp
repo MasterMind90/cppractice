9ca44c7ddd59e582950ede811fbf4cb4a6a8ab02
Amjad Alhouseini <amjadm2008@gmail.com>
2020-12-09 16:32:00 +0200

    Add solution for problem `B - Pasha Maximizes` Link: https://codeforces.com/contest/435/problem/B Tags: greedy, *1400 Ref: https://codeforces.com/contest/435/submission/100772341

---
+++ codeforces/435/B.cpp
@@ -0,0 +1,76 @@
+#ifndef LOCAL
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimization ("unroll-loops")
+#endif
+#include "bits/stdc++.h"
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+#include <ext/pb_ds/detail/standard_policies.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define sim template < class c
+#define int long long
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << '\n'; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+#define fastio ios_base::sync_with_stdio(false);cin.tie(0);
+typedef tree< pair<int,int>, null_type, less<pair<int,int> >, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+typedef long long ll;
+const ll MOD = 1e9 + 7 ;
+const ll N = 2e5 + 10 ;
+const ll INF = 1e18 + 10 ;
+
+signed main(){
+    fastio
+    string s ; 
+    cin >> s ;
+    int n = (int) s.size() ;
+    int k ;
+    cin >> k ;
+    for(int i = 0; i < n; i++){
+    	if ( s[i] == '9' ) continue ;
+    	for(char c = '9'; c > s[i] ; c--){
+    		bool ok = false ;
+	    	for(int j = i + 1; j < n && j - i <= k; j++){
+	    		if ( s[j] == c ) {
+	    			int t = j ;
+	    			while(t > i){
+	    				swap(s[t], s[t - 1]) ;
+	    				t-- ;
+	    			}
+	    			k -= (j - i) ;
+	    			ok = true ;
+	    			break ;
+	    		}
+	    	}
+	    	if ( ok ) break ;
+    	}
+    }
+    cout << s << endl;
+    return 0; 
+}
