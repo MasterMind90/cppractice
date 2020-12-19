ca5f39ac329caddfe1b8b4bec310f9ab713a10e3
Amjad Alhouseini <amjadm2008@gmail.com>
2020-12-19 13:04:00 +0200

    Add solution for problem `B - Move and Turn` Link: https://codeforces.com/contest/1459/problem/B Tags: dp, math, *1300 Ref: https://codeforces.com/contest/1459/submission/101755498

---
+++ codeforces/1459/B.cpp
@@ -0,0 +1,90 @@
+#ifndef LOCAL
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimization ("unroll-loops")
+#endif
+#include "bits/stdc++.h"
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
+#include <ext/pb_ds/detail/standard_policies.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define sim template < class c
+#define int long long
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << '\n'; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+#define fastio ios_base::sync_with_stdio(false);cin.tie(0);
+typedef tree< pair<int,int>, null_type, less<pair<int,int> >, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+typedef long long ll;
+const ll MOD = 1e9 + 7 ;
+const ll N = 2e5 + 10 ;
+const ll INF = 1e18 + 10 ;
+set<pair<int,int> > s ;
+// 1 left
+// 2 right
+// 3 up
+// 4 down
+void dfs(int x, int y, int cur,int dir){
+	// debug() << imie(x) imie(y) imie(cur) imie(dir) ;
+	if ( cur == 0 ) {
+		s.emplace(x, y) ;
+		return ;
+	}
+	if ( dir == 0 ){
+		dfs(x - 1, y, cur - 1, 1) ; // left
+		dfs(x + 1, y, cur - 1, 2) ; // right
+		dfs(x, y + 1, cur - 1, 3) ; // up
+		dfs(x, y - 1, cur - 1, 4) ; // down
+	}
+	else if ( dir == 1 || dir == 2 ) {
+		dfs(x, y + 1, cur - 1, 3) ; // up
+		dfs(x, y - 1, cur - 1, 4) ; // down
+	}
+	else if ( dir == 3 || dir == 4 ) {
+		dfs(x - 1, y, cur - 1, 1) ; // left
+		dfs(x + 1, y, cur - 1, 2) ; // right
+	}
+}
+int fast(int a, int b){
+	if ( b == 0 ) return 1 ;
+	if ( b == 1 ) return a ;
+	int y = fast(a, b / 2 ) ;
+	y *= y ;
+	if ( b & 1 ) {
+		return y * a ;
+	}
+	return y ;
+}
+signed main(){
+    fastio
+    int n ;
+    cin >> n ;
+    int ans = 1 + (((3 * n * (n + 4)) + 2 - ((fast(-1, n)) * ((n * (n + 4)) + 2))) / 8) ;
+    cout << ans << endl;
+    return 0; 
+}
